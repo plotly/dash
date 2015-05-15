0b05b9283a282c530e4523a104294cd9dd5d35d9
Chris P <christopher.parmer@gmail.com>
2015-05-15 00:06:26 -0400

    refactor

--- templates/dash/banner.html
+++
@@ -1,1 +0,0 @@
-<H1>Hello World</H1>
--- utils/utils.py
+++
@@ -1,5 +0,0 @@
-def write_templates(fn, blocks, layout='base.html'):
-    for block in blocks:
-        with open('templates/dash/' + block + '.html', 'w') as f:
-            template = '\n'.join(blocks[block])
-            f.write(template)
