12c06170fe9718c7ba320ef3607c8dd319cf1b08
chriddyp <chris@plot.ly>
2016-04-26 17:52:10 -0400

    move createTreePath to utils

--- renderer/src/reducers/layout.js
+++ renderer/src/reducers/layout.js
@@ -4,20 +4,14 @@ import Immutable from 'immutable';
 // TODO: this should be a prop of the high-level component
 import spec from '../spec.js'; // do we need this now?
 import {ACTIONS} from '../actions';
-
-const pad = R.curry((array, paddingValue) => array.reduce((r, v) => {
-    r.push(paddingValue);
-    r.push(v);
-    return r;
-}, []));
-const createTreePath = (array) => pad(array, 'children');
+import utils from './utils.js';
 
 const layout = (state = Immutable.fromJS(spec), action) => {
     switch (action.type) {
 
         // Update the props of the component
         case ACTIONS('ON_PROP_CHANGE'): {
-            const path = createTreePath(action.payload.itempath);
+            const path = utils.createTreePath(action.payload.itempath);
             path.push('props');
             state = state.mergeIn(path, action.payload.props);
             return state;
