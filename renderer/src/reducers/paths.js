e72783ba2ec8560e1e29775f9d3001d67593efca
chriddyp <chris@plot.ly>
2016-04-26 17:40:54 -0400

    :wrench: cache a copy of the layout paths to the components in the store
    (we’ll need this in a subsequent commit)

---
+++ renderer/src/reducers/paths.js
@@ -0,0 +1,20 @@
+import utils from './utils.js'
+
+import spec from '../spec.js'; // TODO: this'll eventually load from the API
+
+const initialPaths = {};
+
+utils.crawlLayout(spec, (child, itempath) => {
+    if(child.props.id) {
+        initialPaths[child.props.id] = utils.createTreePath(itempath);
+    }
+});
+
+const paths = (state = initialPaths, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default paths;
