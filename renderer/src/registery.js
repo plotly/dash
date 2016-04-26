8b64bcc4ab68e0322f6f83d17730abf1c8752c4c
chriddyp <chris@plot.ly>
2016-04-26 17:49:36 -0400

    :wrench: register new InputControl

--- renderer/src/registery.js
+++ renderer/src/registery.js
@@ -4,9 +4,11 @@ import Header from './components/Header.react';
 import EditableContent from './components/core/EditableContent.react';
 
 import EditableDiv from './components/EditableDiv.react';
+import InputControl from './components/InputControl.react';
 
 export default {
     Header,
     EditableContent,
-    EditableDiv
+    EditableDiv,
+    InputControl
 };
