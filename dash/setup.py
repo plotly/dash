67770ff36f6282bf0cd05e6436e753e80da73b25
chriddyp <chris@plot.ly>
2016-06-29 16:46:25 -0700

    update python package setup

--- dash/setup.py
+++ dash/setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='dash.ly',
-    version='0.10.9',
+    version='0.11.6',
     author='chris p',
     author_email='chris@plot.ly',
-    packages=['dash'],
-    license='See LICENSE.txt',
+    packages=['dash', 'dash/development'],
+    license='MIT',
     description='',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    install_requires=['flask', 'plotly', 'flask-cors']
 )
