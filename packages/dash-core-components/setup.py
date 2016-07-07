a148792f69ab0da53663fe5dd1eb594997f414ae
Per Nilsson <per@sproutlab.com>
2016-07-07 14:24:13 -0700

    Merge pull request #9 from plotly/fix-metadata-path
    :bug: Fix issue with path to component metadata

--- packages/dash-core-components/setup.py
+++ packages/dash-core-components/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='dash_core_components',
-    version='0.1.0',
+    version='0.1.1',
     author='plotly',
     packages=['dash_core_components'],
+    package_data={'dash_core_components': ['../lib/metadata.json']},
     license='MIT',
     description='Dash UI component suite',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
