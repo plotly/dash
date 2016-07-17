7ace9e0b036e2ddadca2aab959c0fed67528562e
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-17 09:40:13 -0700

    Bump pypi to v0.2.2

--- packages/dash-html-components/setup.py
+++ packages/dash-html-components/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash_html_components',
-    version='0.2.1',
+    version='0.2.2',
     author='plotly',
     packages=['dash_html_components'],
     package_data={'dash_html_components': ['metadata.json']},
