52138f389d5b8ef3e2cd25179716b36170b94242
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-17 09:39:30 -0700

    Load metadata path from current directory

--- packages/dash-html-components/dash_html_components/__init__.py
+++ packages/dash-html-components/dash_html_components/__init__.py
@@ -5,7 +5,7 @@ import sys as _sys
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 _dash.development.component_loader.load_components(
-    os.path.join(current_path, '../lib/metadata.json'),
+    os.path.join(current_path, 'metadata.json'),
     ['content', 'id', 'key', 'className', 'style', 'dependencies'],
     globals(),
     _sys._getframe(1).f_globals.get('__name__', '__main__')
