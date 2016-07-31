eed7de73b7d7f2cddf5d5c63cda1b6f3074f86ac
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 11:36:50 -0700

    Resolve components from `window[namespace]`

--- renderer/src/registry.js
+++ renderer/src/registry.js
@@ -19,5 +19,22 @@ export default {
     P,
     Pre,
     Span,
-    Strong
+    Strong,
+
+    resolve: (componentName, namespace) => {
+        const ns = window[namespace];
+
+        if (ns) {
+            if (ns[componentName]) {
+                return ns[componentName];
+            }
+
+            throw new Error(`Component ${componentName} not found in
+                ${namespace}`);
+        }
+
+        throw new Error(`${namespace} was not found, make sure to
+            \`pip install ${namespace}\` and pass it as string in the
+            \`component_suites\` kwarg to \`dash.run_server\`.`);
+    }
 };
