c8464e50ad7564f4a9a8542c7beb17221f4bf143
chriddyp <chris@plot.ly>
2017-02-15 18:08:59 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash.ly',
-    version='0.11.7',
+    version='0.12.0',
     author='chris p',
     author_email='chris@plot.ly',
     packages=['dash', 'dash/development'],
