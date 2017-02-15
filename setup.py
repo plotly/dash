530530e5ab44e731af0a66a298beb31f9e12935a
chriddyp <chris@plot.ly>
2017-02-15 18:01:16 -0500

    match pypi with npm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash_renderer',
-    version='0.2.0',
+    version='0.2.1',
     author='Chris Parmer',
     author_email='chris@plot.ly',
     packages=['dash_renderer'],
