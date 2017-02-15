770e35209a8d7ca4c22c134c088cdd4dafeb20e4
chriddyp <chris@plot.ly>
2017-02-15 13:48:48 -0500

    v0.3.5

--- packages/dash-html-components/setup.py
+++ packages/dash-html-components/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash_html_components',
-    version='0.3.4',
+    version='0.3.5',
     author='Chris Parmer',
     author_email='chris@plot.ly',
     packages=['dash_html_components'],
