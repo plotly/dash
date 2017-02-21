9153f000beb1c4792e33816a95b544a8119e1e3e
chriddyp <chris@plot.ly>
2017-02-21 00:39:02 -0500

    v0.2.5

--- packages/dash-core-components/setup.py
+++ packages/dash-core-components/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash_core_components',
-    version='0.2.4',
+    version='0.2.5',
     author='Chris Parmer',
     author_email='chris@plot.ly',
     packages=['dash_core_components'],
