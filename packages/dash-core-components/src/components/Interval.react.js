d4b25f1f046e1c69942c88d97daa2470dde297ed
chriddyp <chris@plot.ly>
2017-02-23 15:02:49 -0500

    Basic set interval component

---
+++ packages/dash-core-components/src/components/Interval.react.js
@@ -0,0 +1,68 @@
+import React, {Component, PropTypes} from 'react'; // eslint-disable-line no-unused-vars
+
+export default class Interval extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.setInterval = this.setInterval.bind(this);
+    }
+
+    setInterval(props) {
+        const {interval, fireEvent} = props;
+        this.setState({
+            intervalId: window.setInterval(function intervalFunction(){
+                if (fireEvent) {
+                    fireEvent({event: 'setInterval'});
+                }
+            }, interval)
+        });
+    }
+
+    componentDidMount() {
+        if (this.props.fireEvent) {
+            this.setInterval(this.props);
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!this.props.fireEvent && nextProps.fireEvent) {
+            this.setInterval(nextProps);
+        } else if (
+            !this.props.interval !== nextProps.interval &&
+            this.state.intervalId
+        ) {
+            window.clearInterval(this.state.intervalId);
+            this.setInterval(nextProps);
+        }
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.state.intervalId);
+    }
+
+    render() {
+        return null;
+    }
+}
+
+Interval.propTypes = {
+    /**
+     * This component will fire an event every `interval`
+     * milliseconds with the event name `setInterval`
+     */
+    interval: PropTypes.number,
+
+    /**
+     * Dash assigned callback
+     */
+    fireEvent: PropTypes.function,
+
+    /**
+     * Dash assigned callback
+     */
+    valueChanged: PropTypes.function
+};
+
+Interval.defaultProps = {
+    interval: 1000
+};
