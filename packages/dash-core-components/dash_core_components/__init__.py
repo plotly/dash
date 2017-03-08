b8e4f6e9e9be001e335eabe99f14fd1ddeed1740
chriddyp <chris@plot.ly>
2017-03-08 18:18:12 -0500

    specify local and remote CSS and JS paths

--- packages/dash-core-components/dash_core_components/__init__.py
+++ packages/dash-core-components/dash_core_components/__init__.py
@@ -13,5 +13,31 @@ _components = _dash.development.component_loader.load_components(
 
 _this_module = _sys.modules[__name__]
 
+_js_dist = [
+    {
+        "relative_package_path": "bundle.js",
+        "external_url": (
+            "https://unpkg.com/dash-core-components@{}"
+            "/dash_core_components/bundle.js"
+        ).format(__version__)
+    }
+]
+
+_css_dist = [
+    {
+        "relative_package_path": [
+            "react-select@1.0.0-rc.3.min.css",
+            "rc-slider@6.1.2.css"
+        ],
+        "external_url": [
+            "https://unpkg.com/react-select@1.0.0-rc.3/dist/react-select.min.css",
+            "https://unpkg.com/rc-slider@6.1.2/assets/index.css"
+        ]
+    }
+]
+
+
 for component in _components:
     setattr(_this_module, component.__name__, component)
+    setattr(component, '_js_dist', _js_dist)
+    setattr(component, '_css_dist', _css_dist)
