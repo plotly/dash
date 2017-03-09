fc70d319d9aaa0f652d01d77d82505f02812bf89
chriddyp <chris@plot.ly>
2017-03-08 19:57:27 -0500

    update example

--- dropdown.py
+++ dropdown.py
@@ -3,34 +3,40 @@ import time
 import random
 
 from dash.react import Dash
-from dash_html_components import Div
-from dash_core_components import Dropdown, Label
+from dash_html_components import Div, Label
+from dash_core_components import Dropdown
 
 dash = Dash(__name__)
 
 dash.layout = Div(id='wrapper', content=[
     Dropdown(id='source', options=[
-        { 'value': 'a', 'label': 'Option a' },
-        { 'value': 'b', 'label': 'Option b' }
-    ]),
-    Label(id='target', value='will update')
+        {'value': 'a', 'label': 'Option a'},
+        {'value': 'b', 'label': 'Option b'}
+    ], value='a'),
+    Label(id='target')
 ])
 
 
 def update_target(dropdown_data):
     return {
-        'value': dropdown_data['props']['value']
+        'content': dropdown_data['value']
     }
 
 
 dash.react('target', ['source'])(update_target)
 
+dash.css.append_css({
+    'external_url': (
+        'https://rawgit.com/chriddyp/0247653a7c52feb4c48437e1c1837f75'
+        '/raw/d4f178bc09f253251135aeb2141aa077300d1b3f/dash.css'
+    )
+})
+
+# dash.scripts.config.serve_locally = True
+# dash.css.config.serve_locally = True
+
 if __name__ == "__main__":
     dash.run_server(
         port=8050,
-        debug=True,
-        component_suites=[
-            'dash_core_components',
-            'dash_html_components'
-        ]
+        debug=True
     )
