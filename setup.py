f5d6d84a324fb43609502c991457a89613ff090b
chriddyp <chris@plot.ly>
2017-03-08 20:06:45 -0500

    v0.12.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dash.ly',
-    version='0.12.5',
+    version='0.12.6',
     author='chris p',
     author_email='chris@plot.ly',
     packages=['dash', 'dash/development'],
