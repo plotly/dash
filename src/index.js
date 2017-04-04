51f780fb739d4a68dc42f3a41fb95944643a5185
chriddyp <chris@plot.ly>
2017-04-04 00:03:07 -0400

    promise polyfill

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './AppContainer.react';
+require('es6-promise').polyfill();
+
 
 ReactDOM.render(
     <AppContainer/>,
