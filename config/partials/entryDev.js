af127e608c23d950efa296fe215a9832f36e8600
chriddyp <chris@plot.ly>
2017-04-04 00:02:25 -0400

    fetch polyfill

--- config/partials/entryDev.js
+++ config/partials/entryDev.js
@@ -4,8 +4,9 @@ var partial = require('webpack-partial').default;
 
 module.exports = function (config) {
     return partial(config, {
-        entry: {
-            bundle: './index.js'
-        }
+        entry: {bundle: [
+            'whatwg-fetch',
+             './index.js'
+        ]}
     });
 };
