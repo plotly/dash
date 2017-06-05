8e0b4fcf484ee2518fdb85a32e26d3ead5bc97f5
chriddyp <chris@plot.ly>
2017-06-05 15:51:51 -0400

    include babel polyfill

--- config/partials/entryDev.js
+++ config/partials/entryDev.js
@@ -5,6 +5,7 @@ var partial = require('webpack-partial').default;
 module.exports = function (config) {
     return partial(config, {
         entry: {bundle: [
+            'babel-polyfill',
             'whatwg-fetch',
              './index.js'
         ]}
