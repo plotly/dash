f8ba5241e05907b473e418a80e2de24f29546c21
chriddyp <chris@plot.ly>
2017-06-20 17:29:22 -0400

    rewrite setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,35 @@ setup(
     author_email='chris@plot.ly',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='',
+    description='A Python framework for building reactive web-apps. Developed by Plotly.',
     long_description=open('README.md').read(),
     install_requires=[
         'Flask>=0.12',
         'flask-compress',
         'flask-seasurf',
         'plotly'
+    ],
+    url='https://plot.ly/dash',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Widget Sets'
     ]
 )
