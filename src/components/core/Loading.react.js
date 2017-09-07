9b511ac8748e4378d18158bd45e565de321c53c1
chriddyp <chris@plot.ly>
2017-09-07 15:10:55 -0400

    functional component

--- src/components/core/Loading.react.js
+++ src/components/core/Loading.react.js
@@ -1,20 +1,14 @@
 import {connect} from 'react-redux'
 import {isEmpty} from 'ramda'
-import {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 
-class Loading extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        if (!isEmpty(props.requestQueue)) {
-            return (
-                <div className="_dash-loading-callback"/>
-            )
-        } else {
-            return null;
-        }
+function Loading(props) {
+    if (!isEmpty(props.requestQueue)) {
+        return (
+            <div className="_dash-loading-callback"/>
+        )
+    } else {
+        return null;
     }
 }
 
