885340256a92d2242c622a8e57fe7064890e80cb
chriddyp <chris@plot.ly>
2017-09-08 16:06:38 -0400

    move class based config to a dict-based config
    The class-based config was causing a few issues:
    - It was being shared across instances of dash! This was a surprise to
    me. Check this out:
    ```
    In [1]: class Dash(object):
       ...:     def __init__(self):
       ...:         pass
       ...:     class config:
       ...:         b = 3
       ...:
    
    In [2]: d1 = Dash()
    
    In [3]: d1.config.b = 10
    
    In [4]: d2 = Dash()
    
    In [4]: d2.config.b = 15
    
    In [5]: d1.config.b
    Out[5]: 15
    
    In [6]: d2.config.b
    Out[6]: 15
    ```
    - A dict-based config is just much better to deal with: you can
    `print(app.config)` to see all of the values and defaults and you can
    call dict methods like `update` on it.
    - For backwards compatibility, `config` is a `AttributeDict` which
    allows assignment through `__setattr__` (i.e. “dot” assignment), which
    is how the previous config worked
    - This also corrects the spelling correction to
    `supress_callback_exceptions` but falls back to the original mispelled
    version if the new version doesn’t exist yet (for backwards
    compatibility)

---
+++ dash/_utils.py
@@ -0,0 +1,33 @@
+class AttributeDict(dict):
+    """
+    Dictionary subclass enabling attribute lookup/assignment of keys/values.
+    For example::
+        >>> m = AttributeDict({'foo': 'bar'})
+        >>> m.foo
+        'bar'
+        >>> m.foo = 'not bar'
+        >>> m['foo']
+        'not bar'
+    ``AttributeDict`` objects also provide ``.first()`` which acts like
+    ``.get()`` but accepts multiple keys as arguments, and returns the value of
+    the first hit, e.g.::
+        >>> m = AttributeDict({'foo': 'bar', 'biz': 'baz'})
+        >>> m.first('wrong', 'incorrect', 'foo', 'biz')
+        'bar'
+    """
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            # to conform with __getattr__ spec
+            raise AttributeError(key)
+
+    def first(self, *names):
+        for name in names:
+            value = self.get(name)
+            if value:
+                return value
