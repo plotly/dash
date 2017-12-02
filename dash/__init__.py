a07811a525e8721c6b9c32962b38e427c9c782e9
Aly Sivji <alysivji@gmail.com>
2017-12-02 11:55:30 -0600

    Import submodules into __init__.py for IDE code completion

--- dash/__init__.py
+++ dash/__init__.py
@@ -1,3 +1,7 @@
 from .dash import Dash  # noqa: F401
+from . import authentication  # noqa: F401
+from . import dependencies  # noqa: F401
 from . import development  # noqa: F401
+from . import exceptions  # noqa: F401
+from . import resources  # noqa: F401
 from .version import __version__  # noqa: F401
