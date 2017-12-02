288fdf2a26404dac33f4a12523e308dfae338001
John Bampton <jbampton@users.noreply.github.com>
2017-12-02 10:31:52 +1000

    Use single quotes consistently. The python code in this project uses single quotes except for this file which had both single and double quotes.

--- packages/dash-core-components/dash_core_components/__init__.py
+++ packages/dash-core-components/dash_core_components/__init__.py
@@ -19,32 +19,32 @@ _js_dist = [
         'namespace': 'dash_core_components'
     },
     {
-        "relative_package_path": "bundle.js",
-        "external_url": (
-            "https://unpkg.com/dash-core-components@{}"
-            "/dash_core_components/bundle.js"
+        'relative_package_path': 'bundle.js',
+        'external_url': (
+            'https://unpkg.com/dash-core-components@{}'
+            '/dash_core_components/bundle.js'
         ).format(__version__),
-        "namespace": "dash_core_components"
+        'namespace': 'dash_core_components'
     }
 ]
 
 _css_dist = [
     {
-        "relative_package_path": [
-            "rc-slider@6.1.2.css",
-            "react-select@1.0.0-rc.3.min.css",
-            "react-virtualized@9.9.0.css",
-            "react-virtualized-select@3.1.0.css",
-            "react-dates@12.3.0.css"
+        'relative_package_path': [
+            'rc-slider@6.1.2.css',
+            'react-select@1.0.0-rc.3.min.css',
+            'react-virtualized@9.9.0.css',
+            'react-virtualized-select@3.1.0.css',
+            'react-dates@12.3.0.css'
         ],
-        "external_url": [
-            "https://unpkg.com/react-select@1.0.0-rc.3/dist/react-select.min.css",
-            "https://unpkg.com/react-virtualized@9.9.0/styles.css",
-            "https://unpkg.com/react-virtualized-select@3.1.0/styles.css",
-            "https://unpkg.com/rc-slider@6.1.2/assets/index.css",
-            "https://unpkg.com/dash-core-components@{}/dash_core_components/react-dates@12.3.0.css".format(__version__)
+        'external_url': [
+            'https://unpkg.com/react-select@1.0.0-rc.3/dist/react-select.min.css',
+            'https://unpkg.com/react-virtualized@9.9.0/styles.css',
+            'https://unpkg.com/react-virtualized-select@3.1.0/styles.css',
+            'https://unpkg.com/rc-slider@6.1.2/assets/index.css',
+            'https://unpkg.com/dash-core-components@{}/dash_core_components/react-dates@12.3.0.css'.format(__version__)
         ],
-        "namespace": "dash_core_components"
+        'namespace': 'dash_core_components'
     }
 ]
 
