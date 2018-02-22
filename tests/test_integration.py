a4196c602f879dcc5f35a0eba712bde8b1646792
chriddyp <chris@plot.ly>
2018-02-21 20:07:50 -0500

    add flowtype integration test
    sanity check for https://github.com/plotly/dash/pull/207

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,7 @@
 from multiprocessing import Value
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_flow_example
 from dash import Dash
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
@@ -112,3 +113,37 @@ class Tests(IntegrationTests):
         self.assertEqual(output2.text, initial_output)
         
         assert_clean_console(self)
+
+    def test_flow_component(self):
+        app.layout = html.Div([
+            dash_flow_example.ExampleReactComponent(
+                id='react',
+                value='my-value',
+                label='react component'
+            ),
+            dash_flow_example.ExampleFlowComponent(
+                id='flow',
+                value='my-value',
+                label='flow component'
+            ),
+            html.Hr(),
+            html.Div(id='output')
+        ])
+
+        @app.callback(Output('output', 'children'),
+                      [Input('react', 'value'), Input('flow', 'value')])
+        def display_output(react_value, flow_value):
+            return html.Div([
+                'You have entered {} and {}'.format(react_value, flow_value),
+                html.Hr(),
+                html.Label('Flow Component Docstring'),
+                html.Pre(dash_flow_example.ExampleFlowComponent.__doc__),
+                html.Hr(),
+                html.Label('React PropTypes Component Docstring'),
+                html.Pre(dash_flow_example.ExampleReactComponent.__doc__),
+                html.Div(id='waitfor')
+            ])
+
+        self.startServer(app)
+        self.wait_for_element_by_id('waitfor')
+        self.percy_snapshot(name='flowtype')
