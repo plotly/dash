b317a2802e777568645e676e0741a0730c3a32d2
Alex Marvin <alex.marvin@gmail.com>
2018-06-21 09:01:28 -0400

    Improves cross-browser compatibility
    Adds meta tags recommended by Bootstrap 3.3 basic template for cross-browser compatibility

--- dash/dash.py
+++ dash/dash.py
@@ -300,6 +300,8 @@ class Dash(object):
         <html>
             <head>
                 <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>{}</title>
                 {}
             </head>
