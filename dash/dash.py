13ac0b2e981df4868b29108b57a5e99344bc89e8
Alex Marvin <alex.marvin@gmail.com>
2018-06-25 12:48:01 -0400

    Adds Bootstrap 4.1 suggested viewport

--- dash/dash.py
+++ dash/dash.py
@@ -301,7 +301,7 @@ class Dash(object):
             <head>
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                 <title>{}</title>
                 {}
             </head>
