09f32a6ce24a99ab5311b8cd7ba4ea705396af71
Ryan Marren <rymarr@tuta.io>
2018-08-20 21:34:11 -0400

    Add 'namespace' and 'type' props to _dash_update_component payload.

--- src/actions/index.js
+++ src/actions/index.js
@@ -403,8 +403,18 @@ function updateOutput(
      * }
      *
      */
+
+     const { type: outputType, namespace: outputNamespace } = view(
+      lensPath(paths[outputComponentId]),
+      layout
+    )
      const payload = {
-         output: {id: outputComponentId, property: outputProp}
+         output: {
+           id: outputComponentId,
+           property: outputProp,
+           type: outputType,
+           namespace: outputNamespace,
+         }
      };
 
      if (event) {
