f92812b28515f695797578d623ed1193cadda6aa
Ryan Marren <rymarr@tuta.io>
2018-10-01 22:32:10 -0400

    Make sure children is component before validation.

--- dash/dash.py
+++ dash/dash.py
@@ -945,10 +945,11 @@ class Dash(object):
                     validator.errors, 0, error_message))
         # Must also validate initialization of newly created components
         if component_property == 'children':
-            value.validate()
-            for component in value.traverse():
-                if isinstance(component, Component):
-                    component.validate()
+            if isinstance(value, Component):
+                value.validate()
+                for component in value.traverse():
+                    if isinstance(component, Component):
+                        component.validate()
 
     def _validate_layout(self):
         if self.layout is None:
