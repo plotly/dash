89c5cae41796c8a56d327d6207d6253dd0a71e4c
Ryan Marren <rymarr@tuta.io>
2018-10-10 15:08:51 -0400

    Add ComponentDisabledOverlay

---
+++ src/components/error/ComponentDisabledOverlay.react.js
@@ -0,0 +1,32 @@
+const styles = {
+  root: {
+    position: 'relative'
+  },
+  overlay: {
+    position: 'relative',
+    padding: '2px',
+    backgroundColor: 'rgb(0, 0, 255, .5)',
+    border: '1px solid black',
+    borderRadius: '2px',
+    display: 'inline-block'
+  },
+  childWrapper: {
+    position: 'relative',
+    zIndex: -1
+  }
+}
+
+function ComponentDisabledOverlay({ children }) {
+  return (
+    <div style={styles.root}>
+      <div
+        style={styles.overlay}>
+        <div style={styles.childWrapper}>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default ComponentDisabledOverlay;
