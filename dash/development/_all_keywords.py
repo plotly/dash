903879b447aba51febfd086a2ae4d681c3a4c35f
Ryan Marren <rymarr@tuta.io>
2018-11-05 18:55:30 -0500

    Add set of all python keywords.

---
+++ dash/development/_all_keywords.py
@@ -0,0 +1,44 @@
+# This is a set of Python keywords that cannot be used as prop names.
+# Keywords for a particular version are obtained as follows:
+# >>> import keyword
+# >>> keyword.kwlist
+
+kwlist = set([
+    'and',
+    'elif',
+    'is',
+    'global',
+    'as',
+    'in',
+    'if',
+    'from',
+    'raise',
+    'for',
+    'except',
+    'nonlocal',
+    'pass',
+    'finally',
+    'print',
+    'import',
+    'True',
+    'None',
+    'return',
+    'exec',
+    'await',
+    'else',
+    'break',
+    'not',
+    'with',
+    'class',
+    'assert',
+    'False',
+    'yield',
+    'try',
+    'while',
+    'continue',
+    'del',
+    'async',
+    'or',
+    'def',
+    'lambda'
+])
