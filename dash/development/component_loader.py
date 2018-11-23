2a4ac5a490842547733c77a89f03a024061bc4d1
Ryan Patrick Kyle <ryan@plot.ly>
2018-11-23 14:36:27 -0500

    Changes to support transpiling of JSON>>R within Python

--- dash/development/component_loader.py
+++ dash/development/component_loader.py
@@ -4,7 +4,10 @@ import os
 from .base_component import generate_class
 from .base_component import generate_class_file
 from .base_component import ComponentRegistry
-
+from .base_component import generate_class_file_r
+from .base_component import generate_help_file_r
+from .base_component import generate_export_string_r
+from .base_component import generate_rpkg
 
 def _get_metadata(metadata_path):
     # Start processing
@@ -110,3 +113,74 @@ def generate_classes(namespace, metadata_path='lib/metadata.json'):
             array_string += '    "{:s}",\n'.format(a)
         array_string += ']\n'
         f.write('\n\n__all__ = {:s}'.format(array_string))
+
+def generate_classes_r(namespace, metadata_path='lib/metadata.json', pkgjson_path='package.json'):
+    """Load React component metadata into a format Dash can parse,
+    then create python class files.
+
+    Usage: generate_classes_r()
+
+    Keyword arguments:
+    namespace -- name of the generated python package (also output dir)
+
+    metadata_path -- a path to a JSON file created by
+    [`react-docgen`](https://github.com/reactjs/react-docgen).
+
+    pkgjson_path -- a path to a JSON file created by
+    [`cookiecutter`](https://github.com/audreyr/cookiecutter).
+
+    Returns:
+    """
+
+    data = _get_metadata(metadata_path)
+    pkg_data = _get_metadata(pkgjson_path)
+    imports_path = os.path.join(namespace, '_imports_.py')
+    export_string = ''
+
+    if namespace == 'dash_html_components':
+        prefix = 'html'
+    elif namespace == 'dash_core_components':
+        prefix = 'core'
+    else:
+        prefix = ''
+
+    # Make sure the file doesn't exist, as we use append to write lines
+    if os.path.exists(imports_path):
+        os.remove(imports_path)
+
+    # Remove the R NAMESPACE file if it exists, this will be repopulated
+    if os.path.isfile('NAMESPACE'):
+        os.remove('NAMESPACE')
+
+    # Iterate over each property name (which is a path to the component)
+    for componentPath in data:
+        componentData = data[componentPath]
+
+        # Extract component name from path
+        # e.g. src/components/MyControl.react.js
+        # TODO Make more robust - some folks will write .jsx and others
+        # will be on windows. Unfortunately react-docgen doesn't include
+        # the name of the component atm.
+        name = componentPath.split('/').pop().split('.')[0]
+
+        export_string += generate_export_string_r(name, prefix)
+        
+        generate_class_file_r(
+            name,
+            componentData['props'],
+            componentData['description'],
+            namespace,
+            prefix
+        )
+        
+        generate_help_file_r(
+            name,
+            componentData['props'],
+            prefix
+        )
+
+    generate_rpkg(
+        pkg_data,
+        namespace,
+        export_string
+    )
