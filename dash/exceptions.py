2e7533ec86ecdbe9d60c40fd882948e09c046027
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-12-10 15:54:02 -0500

    Add ResourceException.

--- dash/exceptions.py
+++ dash/exceptions.py
@@ -65,8 +65,14 @@ class InvalidConfig(DashException):
 class InvalidResourceError(DashException):
     pass
 
+
 class InvalidIndexException(DashException):
     pass
 
+
 class DependencyException(DashException):
+    pass
+
+
+class ResourceException(DashException):
     pass
