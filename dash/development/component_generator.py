0fc3d2d7b1573dbc11f2e2b6b7d815cc161b7071
Ryan Patrick Kyle <ryan@plot.ly>
2018-12-17 16:18:57 -0500

    make warning msg for component prefix more explicit

--- dash/development/component_generator.py
+++ dash/development/component_generator.py
@@ -43,7 +43,7 @@ def generate_components(components_source, project_shortname,
         ]
         prefix = s[-1]
         print(
-            'Warning: a component prefix was '
+            'Warning: a DashR component prefix was '
             'not provided. Using `{}`.'.format(prefix),
             file=sys.stderr
         )
