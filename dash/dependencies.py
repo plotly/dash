cb2477a230ee25881396398b149d96fdbf402ad6
Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>
2019-02-08 09:07:28 -0500

    Update dependencies.py

--- dash/dependencies.py
+++ dash/dependencies.py
@@ -1,18 +1,18 @@
-# pylint: disable=old-style-class, too-few-public-methods
+# pylint: disable=too-few-public-methods
 class Output:
     def __init__(self, component_id, component_property):
         self.component_id = component_id
         self.component_property = component_property
 
 
-# pylint: disable=old-style-class, too-few-public-methods
+# pylint: disable=too-few-public-methods
 class Input:
     def __init__(self, component_id, component_property):
         self.component_id = component_id
         self.component_property = component_property
 
 
-# pylint: disable=old-style-class, too-few-public-methods
+# pylint: disable=too-few-public-methods
 class State:
     def __init__(self, component_id, component_property):
         self.component_id = component_id
