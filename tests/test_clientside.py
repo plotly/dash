0ee3f453c695fad2bf958296d11072c836690abf
Chris P <chris@plot.ly>
2019-03-30 15:29:51 -0400

    :link: test chained assertions

--- tests/test_clientside.py
+++ tests/test_clientside.py
@@ -76,3 +76,108 @@ class Tests(IntegrationTests):
                 'Client says "hello world"'
             )
         )
+
+
+    def test_chained_serverside_clientside_callbacks(self):
+        app = dash.Dash(__name__, assets_folder='test_clientside')
+
+        app.layout = html.Div([
+
+            html.Label('x'),
+            dcc.Input(id='x', value=3),
+
+            html.Label('y'),
+            dcc.Input(id='y', value=6),
+
+            # clientside
+            html.Label('x + y (clientside)'),
+            dcc.Input(id='x+y'),
+
+            # server-side
+            html.Label('x+y / 2 (serverside)'),
+            dcc.Input(id='x+y / 2'),
+
+            # server-side
+            html.Div([
+                html.Label('Display x, y, x+y/2 (serverside)'),
+                dcc.Textarea(id='display-all-of-the-values'),
+            ]),
+
+            # clientside
+            html.Label('Mean(x, y, x+y, x+y/2) (clientside)'),
+            dcc.Input(id='mean-of-all-values'),
+
+        ])
+
+        app.callback(
+            Output('x+y', 'value'),
+            [Input('x', 'value'),
+             Input('y', 'value')],
+            client_function=ClientFunction('R', 'add'))
+
+        call_counts = {
+            'divide': Value('i', 0),
+            'display': Value('i', 0)
+        }
+
+        @app.callback(Output('x+y / 2', 'value'),
+                      [Input('x+y', 'value')])
+        def divide_by_two(value):
+            call_counts['divide'].value += 1
+            return float(value) / 2.0
+
+        @app.callback(Output('display-all-of-the-values', 'value'),
+                      [Input('x', 'value'),
+                       Input('y', 'value'),
+                       Input('x+y', 'value'),
+                       Input('x+y / 2', 'value')])
+        def display_all(*args):
+            call_counts['display'].value += 1
+            return '\n'.join([str(a) for a in args])
+
+        app.callback(
+            Output('mean-of-all-values', 'value'),
+            [Input('x', 'value'), Input('y', 'value'),
+             Input('x+y', 'value'), Input('x+y / 2', 'value')],
+            client_function=ClientFunction('clientside', 'mean'))
+
+        self.startServer(app)
+
+        def run_assertions(assertions):
+            for id in assertions:
+                WebDriverWait(self.driver, 10).until(
+                    EC.text_to_be_present_in_element_value(
+                        (By.ID, id),
+                        assertions[id]
+                    ),
+                    message='Failed assertion: #{}.value != {}'.format(
+                        id, assertions[id]
+                    )
+                )
+
+        run_assertions({
+            'x': '3',
+            'y': '6',
+            'x+y': '9',
+            'x+y / 2': '4.5',
+            'display-all-of-the-values': '3\n6\n9\n4.5',
+            'mean-of-all-values': str((3+6+9+4.5) / 4.0)
+        })
+        self.assertEqual(call_counts['display'].value, 1)
+        self.assertEqual(call_counts['divide'].value, 1)
+
+        x_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'x'))
+        )
+        x_input.send_keys('1')
+
+        run_assertions({
+            'x': '31',
+            'y': '6',
+            'x+y': '37',
+            'x+y / 2': '18.5',
+            'display-all-of-the-values': '31\n6\n37\n18.5',
+            'mean-of-all-values': str((31+6+37+18.5) / 4.0)
+        })
+        self.assertEqual(call_counts['display'].value, 2)
+        self.assertEqual(call_counts['divide'].value, 2)
--- tests/test_clientside/clientside.js
+++ tests/test_clientside/clientside.js
@@ -2,6 +2,10 @@ window.clientside = {
 
     display: function (value) {
         return 'Client says "' + value + '"';
+    },
+
+    mean: function (...args) {
+        return R.mean(args);
     }
 
 }
