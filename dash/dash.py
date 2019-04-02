e8ac908591aab26b9a81037b3b326a4cf33aa90b
Kevin Mader <kmader@magicleap.com>
2019-04-02 15:51:29 +0200

    adding auto_callback

--- dash/dash.py
+++ dash/dash.py
@@ -13,6 +13,7 @@ import warnings
 import re
 import logging
 import pprint
+import inspect
 
 from functools import wraps
 
@@ -1030,6 +1031,66 @@ class Dash(object):
             return add_context
 
         return wrap_func
+
+    # Auto_callback uses inspect to populate input and output fields
+    def auto_callback(self, debug=False):
+
+        valid_prefix_names = ['update_', 'callback_']
+
+        def check_prefix(in_name):
+            for prefix_name in valid_prefix_names:
+                if prefix_name in in_name:
+                    return in_name.find(prefix_name) + len(prefix_name)
+
+            raise ValueError('EasyDash requires callback name to start with {}'.format(valid_prefix_names))
+
+        def process_output(callback_func):
+            callback_name = getattr(callback_func, '__name__', '')
+            comp_prop_start_idx = check_prefix(callback_name)
+            comp_prop_name = callback_name[comp_prop_start_idx:]
+            property_start = comp_prop_name.rfind('_')
+            if property_start >= 1:
+                prop_name = comp_prop_name[property_start + 1:]
+                comp_name = comp_prop_name[:property_start]
+            else:
+                # assume children if no underscore
+                comp_name = comp_prop_name
+                prop_name = 'children'
+            return Output(component_id=comp_name, component_property=prop_name)
+
+        def process_input(callback_func):
+            if sys.version_info[0] == 3:
+                spec_args = inspect.getfullargspec(callback_func).args
+            elif sys.version_info[0] == 2:
+                spec_args = inspect.getargspec(callback_func).args
+            else:
+                raise ValueError("Requires Python 2 or 3, {}".format(sys.version_info))
+
+            input_list = []
+            for c_comp_prop_arg in spec_args:
+                property_start = c_comp_prop_arg.rfind('_')
+                if property_start >= 1:
+                    comp_name = c_comp_prop_arg[:property_start]
+                    prop_name = c_comp_prop_arg[property_start + 1:]
+                else:
+                    # assume value if no underscore
+                    comp_name = c_comp_prop_arg
+                    prop_name = 'value'
+                input_list += [Input(component_id=comp_name, component_property=prop_name)]
+            return input_list
+
+        def wrap_callback(callback_func):
+            # process outputs
+            output = process_output(callback_func)
+            if debug:
+                print('Output:', output)
+            # process inputs
+            inputs = process_input(callback_func)
+            if debug:
+                print('Inputs:', inputs)
+            return self.callback(output, inputs=inputs)(callback_func)
+
+        return wrap_callback
 
     def dispatch(self):
         body = flask.request.get_json()
