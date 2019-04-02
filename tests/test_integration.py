0aac5dd49a69eb7ff2d3730bb29359e2bf432661
Kevin Mader <kmader@magicleap.com>
2019-04-02 17:14:16 +0200

    adding integration test for auto_callback

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -89,6 +89,62 @@ class Tests(IntegrationTests):
 
         assert_clean_console(self)
 
+    def test_auto_callback(self):
+        app = dash.Dash(__name__)
+        app.layout = html.Div([
+            dcc.Input(
+                id='input',
+                value='initial value'
+            ),
+            html.Div(
+                html.Div([
+                    1.5,
+                    None,
+                    'string',
+                    html.Div(id='output1')
+                ])
+            )
+        ])
+
+        call_count = Value('i', 0)
+
+        @app.auto_callback()
+        def update_output1_children(input_value):
+            call_count.value = call_count.value + 1
+            return value
+
+        self.startServer(app)
+
+        self.wait_for_text_to_equal('#output1', 'initial value')
+        self.percy_snapshot(name='auto-callback-1')
+
+        input1 = self.wait_for_element_by_id('input')
+
+        chain = (ActionChains(self.driver)
+                 .click(input1)
+                 .send_keys(Keys.HOME)
+                 .key_down(Keys.SHIFT)
+                 .send_keys(Keys.END)
+                 .key_up(Keys.SHIFT)
+                 .send_keys(Keys.DELETE))
+        chain.perform()
+
+        input1.send_keys('hello world')
+
+        self.wait_for_text_to_equal('#output1', 'hello world')
+        self.percy_snapshot(name='auto-callback-2')
+
+        self.assertEqual(
+            call_count.value,
+            # an initial call to retrieve the first value
+            # and one for clearing the input
+            2 +
+            # one for each hello world character
+            len('hello world')
+        )
+
+        assert_clean_console(self)
+
     def test_wildcard_callback(self):
         app = dash.Dash(__name__)
         app.layout = html.Div([
