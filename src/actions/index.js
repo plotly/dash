fc49968f849e2b8ed97b64ad18fb402e6ad8a727
Chris P <chris@plot.ly>
2019-04-08 11:23:58 -0700

    :zap: simpler API check as according to https://github.com/plotly/dash/pull/672/commits/7281b7d5c12eb13dd93cdee20ce762d1e7053466

--- src/actions/index.js
+++ src/actions/index.js
@@ -500,11 +500,7 @@ function updateOutput(
     }
 
     // Clientside hook
-    if (
-        clientside_function &&
-        // allow the API to skip clientside if provided {} or nothing at all
-        !isEmpty(clientside_function)
-    ) {
+    if (clientside_function) {
         let returnValue;
         try {
             returnValue = window.dash_clientside[clientside_function.namespace][
