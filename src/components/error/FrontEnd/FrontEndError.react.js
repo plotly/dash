3d7711b1f733ea1b386d77b0a7096fe3651bd668
Chris P <chris@plot.ly>
2019-04-10 19:42:27 -0700

    embed backend errors as iframes in the FrontEndError container

--- src/components/error/FrontEnd/FrontEndError.react.js
+++ src/components/error/FrontEnd/FrontEndError.react.js
@@ -5,6 +5,8 @@ import CloseIcon from '../icons/CloseIcon.svg';
 import CollapseIcon from '../icons/CollapseIcon.svg';
 import PropTypes from 'prop-types';
 
+import werkzeugCss from '../werkzeug.css.txt';
+
 class FrontEndError extends Component {
     constructor(props) {
         super(props);
@@ -47,149 +49,75 @@ class FrontEndError extends Component {
             </div>
         ) : (
             <div className={cardClasses}>
-                <div className="dash-fe-error-top">
-                    <ErrorIcon className="dash-fe-error__icon-error" />
-                    <h6 className="dash-fe-error__title">
-                        {e.error.message ||
-                            'An error was thrown that was not an Error object, so info could not be gathered.'}
-                    </h6>
-                    {this.props.isListItem ? (
-                        <CollapseIcon
-                            className="dash-fe-error__collapse dash-fe-error__collapse--flipped"
-                            onClick={() => this.setState({collapsed: true})}
-                        />
-                    ) : (
-                        closeButton
-                    )}
-                </div>
-                {!e.error.stack ? null: (
-                    <div className="dash-fe-error__st">
-                        {e.error.stack &&
-                            e.error.stack.split('\n').map(line => <p>{line}</p>)}
-                    </div>
-                )}
-                {!e.error.html ? null : (
-                    <div className="dash-be-error__st">
-                        <div className="dash-backend-error">
-                            {/* Embed werkzeug debugger in an iframe to prevent
-                                CSS leaking - werkzeug HTML includes a bunch
-                                of CSS on base html elements like `<body/>`
-                              */}
-                            <iframe
-                                style={{
-                                    /*
-                                     * 67px of padding and margin between this
-                                     * iframe and the parent container.
-                                     * 67 was determined manually in the
-                                     * browser's dev tools.
-                                     */
-                                    'width': 'calc(600px - 67px)',
-                                    'height': '75vh',
-                                    'border': 'none'
-                                }}
-                                srcDoc={e.error.html.replace(
-                                '</head>',
-`
-<style type="text/css">
-    {
-        font-family: Roboto;
-    }
-    .traceback {
-        background-color: white;
-        border: 2px solid #dfe8f3;
-        border-radius: 0px 0px 4px 4px;
-        color: #506784;
-    }
-    h2.traceback {
-        background-color: #f3f6fa;
-        border: 2px solid #dfe8f3;
-        border-bottom: 0px;
-        box-sizing: border-box;
-        border-radius: 4px 4px 0px 0px;
-        color: #506784;
-    }
-    h2.traceback em{
-        color: #506784;
-        font-weight: 100;
-    }
-    .traceback pre, .debugger textarea{
-        background-color: #F3F6FA;
-    }
-    .debugger h1{
-        color: #506784;
-        font-family: Roboto;
-    }
-    .explanation {
-        color: #A2B1C6;
-    }
-     /* Hide the Don't Panic! footer */
-     .debugger .footer {
-         display: none;
-     }
+                {errorHeader}
 
-    /* Messing around */
-     .traceback > ul > li {
-         display: none;
-     }
-     .traceback > ul > li:nth-last-child(-n+3) {
-         display: block;
-     }
-     .debugger h1 {
-         display: none;
-     }
+                <ErrorContent error={e.error}/>
 
-     .debugger .errormsg {
-         margin: 0;
-         color: #506784;
-         font-size: 16px;
-         background-color: #f3f6fa;
-         border: 2px solid #dfe8f3;
-         box-sizing: border-box;
-         border-radius: 4px;
-         padding: 10px;
-     }
-
-    .debugger .pastemessage input {
-        display: none;
-    }
-
-    .debugger .explanation {
-        display: none;
-    }
-    .debugger div.plain {
-        border-radius: 4px;
-        border-width: 2px;
-        color: #506784;
-    }
-
-    body {
-        padding: 0px;
-        margin: 0px;
-    }
-
-    .plain {
-        display: block !important;
-    }
-    .plain > form > p {
-        display: none;
-    }
-    .plain pre {
-        padding: 15px !important;
-        overflow-x: scroll;
-    }
-</style>
-</head>`
-                            )}/>
-                        </div>
-                    </div>
-                )}
             </div>
         );
     }
 }
 
+function ErrorContent({error}) {
+    return (
+        <div className='error-container'>
+        {/* Frontend Error objects */}
+        {!error.stack ? null: (
+            <div className="dash-fe-error__st">
+                {error.stack.split('\n').map(line => <p>{line}</p>)}
+            </div>
+        )}
+
+        {/* Backend Error */}
+        {!error.html ? null : (
+            <div className="dash-be-error__st">
+                <div className="dash-backend-error">
+                    {/* Embed werkzeug debugger in an iframe to prevent
+                        CSS leaking - werkzeug HTML includes a bunch
+                        of CSS on base html elements like `<body/>`
+                      */}
+
+                    <iframe
+                        srcDoc={error.html.replace(
+                            '</head>',
+                            `<style type="text/css">${werkzeugCss}</style></head>`
+                        )}
+                        style={{
+                            /*
+                             * 67px of padding and margin between this
+                             * iframe and the parent container.
+                             * 67 was determined manually in the
+                             * browser's dev tools.
+                             */
+                            'width': 'calc(600px - 67px)',
+                            'height': '75vh',
+                            'border': 'none'
+                        }}
+                    />
+                </div>
+            </div>
+        )}
+        </div>
+    );
+}
+
+
 FrontEndError.propTypes = {
-    e: PropTypes.object,
+    e: PropTypes.shape({
+        myUID: PropTypes.string,
+        timestamp: PropTypes.object,
+        error: PropTypes.shape({
+            message: PropTypes.string,
+
+            /* front-end error messages */
+            stack: PropTypes.string,
+
+            /* backend error messages */
+            html: PropTypes.string,
+
+
+        })
+    }),
     resolve: PropTypes.func,
     inAlertsTray: PropTypes.bool,
     isListItem: PropTypes.bool,
