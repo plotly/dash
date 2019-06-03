4005368bbc21beaa07a6f2c369437747831b49c7
byron <byron@plot.ly>
2019-06-03 15:04:36 -0400

    :white_check_mark: refactoring on devtools with new fixtures

---
+++ tests/integration/devtools/test_devtools_error_handling.py
@@ -0,0 +1,202 @@
+# -*- coding: UTF-8 -*-
+import dash_html_components as html
+import dash_core_components as dcc
+
+import dash
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+
+def test_dev001_python_errors(dash_duo):
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(
+        [
+            html.Button(id="python", children="Python exception", n_clicks=0),
+            html.Div(id="output"),
+        ]
+    )
+
+    @app.callback(Output("output", "children"), [Input("python", "n_clicks")])
+    def update_output(n_clicks):
+        if n_clicks == 1:
+            1 / 0
+        elif n_clicks == 2:
+            raise Exception("Special 2 clicks exception")
+
+    dash_duo.start_app_server(
+        app,
+        debug=True,
+        use_reloader=False,
+        use_debugger=True,
+        dev_tools_hot_reload=False,
+    )
+
+    dash_duo.percy_snapshot("devtools - python exception - start")
+
+    dash_duo.find_element("#python").click()
+    dash_duo.wait_for_text_to_equal(".test-devtools-error-count", "1")
+    dash_duo.percy_snapshot("devtools - python exception - closed")
+
+    dash_duo.find_element(".test-devtools-error-toggle").click()
+    dash_duo.percy_snapshot("devtools - python exception - open")
+
+    dash_duo.find_element(".test-devtools-error-toggle").click()
+    dash_duo.find_element("#python").click()
+    dash_duo.wait_for_text_to_equal(".test-devtools-error-count", "2")
+    dash_duo.percy_snapshot("devtools - python exception - 2 errors")
+
+    dash_duo.find_element(".test-devtools-error-toggle").click()
+    dash_duo.percy_snapshot("devtools - python exception - 2 errors open")
+
+
+def test_dev002_prevent_update_not_in_error_msg(dash_duo):
+    # raising PreventUpdate shouldn't display the error message
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(
+        [
+            html.Button(id="python", children="Prevent update", n_clicks=0),
+            html.Div(id="output"),
+        ]
+    )
+
+    @app.callback(Output("output", "children"), [Input("python", "n_clicks")])
+    def update_output(n_clicks):
+        if n_clicks == 1:
+            raise PreventUpdate
+        if n_clicks == 2:
+            raise Exception("An actual python exception")
+
+        return "button clicks: {}".format(n_clicks)
+
+    dash_duo.start_app_server(
+        app,
+        debug=True,
+        use_reloader=False,
+        use_debugger=True,
+        dev_tools_hot_reload=False,
+    )
+
+    for _ in range(3):
+        dash_duo.find_element("#python").click()
+
+    assert (
+        dash_duo.find_element("#output").text == "button clicks: 3"
+    ), "the click counts correctly in output"
+
+    # two exceptions fired, but only a single exception appeared in the UI:
+    # the prevent default was not displayed
+    dash_duo.wait_for_text_to_equal(".test-devtools-error-count", "1")
+    dash_duo.percy_snapshot(
+        "devtools - prevent update - only a single exception"
+    )
+
+
+def test_dev003_validation_errors_in_place(dash_duo):
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(
+        [
+            html.Button(id="button", children="update-graph", n_clicks=0),
+            dcc.Graph(id="output", figure={"data": [{"y": [3, 1, 2]}]}),
+        ]
+    )
+
+    # animate is a bool property
+    @app.callback(Output("output", "animate"), [Input("button", "n_clicks")])
+    def update_output(n_clicks):
+        if n_clicks == 1:
+            return n_clicks
+
+    dash_duo.start_app_server(
+        app,
+        debug=True,
+        use_reloader=False,
+        use_debugger=True,
+        dev_tools_hot_reload=False,
+    )
+
+    dash_duo.find_element("#button").click()
+    dash_duo.wait_for_text_to_equal(".test-devtools-error-count", "1")
+    dash_duo.percy_snapshot("devtools - validation exception - closed")
+
+    dash_duo.find_element(".test-devtools-error-toggle").click()
+    dash_duo.percy_snapshot("devtools - validation exception - open")
+
+
+def test_dev004_validation_errors_creation(dash_duo):
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(
+        [
+            html.Button(id="button", children="update-graph", n_clicks=0),
+            html.Div(id="output"),
+        ]
+    )
+
+    # animate is a bool property
+    @app.callback(Output("output", "children"), [Input("button", "n_clicks")])
+    def update_output(n_clicks):
+        if n_clicks == 1:
+            return dcc.Graph(
+                id="output", animate=0, figure={"data": [{"y": [3, 1, 2]}]}
+            )
+
+    dash_duo.start_app_server(
+        app,
+        debug=True,
+        use_reloader=False,
+        use_debugger=True,
+        dev_tools_hot_reload=False,
+    )
+
+    dash_duo.find_element("#button").click()
+    dash_duo.wait_for_text_to_equal(".test-devtools-error-count", "1")
+    dash_duo.percy_snapshot("devtools - validation creation exception - closed")
+
+    dash_duo.find_element(".test-devtools-error-toggle").click()
+    dash_duo.percy_snapshot("devtools - validation creation exception - open")
+
+
+def test_dev005_multiple_outputs(dash_duo):
+    app = dash.Dash(__name__)
+    app.layout = html.Div(
+        [
+            html.Button(
+                id="multi-output",
+                children="trigger multi output update",
+                n_clicks=0,
+            ),
+            html.Div(id="multi-1"),
+            html.Div(id="multi-2"),
+        ]
+    )
+
+    @app.callback(
+        [Output("multi-1", "children"), Output("multi-2", "children")],
+        [Input("multi-output", "n_clicks")],
+    )
+    def update_outputs(n_clicks):
+        if n_clicks == 0:
+            return [
+                "Output 1 - {} Clicks".format(n_clicks),
+                "Output 2 - {} Clicks".format(n_clicks),
+            ]
+        else:
+            n_clicks / 0
+
+    dash_duo.start_app_server(
+        app,
+        debug=True,
+        use_reloader=False,
+        use_debugger=True,
+        dev_tools_hot_reload=False,
+    )
+
+    dash_duo.find_element("#multi-output").click()
+    dash_duo.wait_for_text_to_equal(".test-devtools-error-count", "1")
+    dash_duo.percy_snapshot("devtools - multi output python exception - closed")
+
+    dash_duo.find_element(".test-devtools-error-toggle").click()
+    dash_duo.percy_snapshot("devtools - multi output python exception - open")
---
+++ tests/integration/devtools/test_props_check.py
@@ -0,0 +1,243 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash
+from dash.dependencies import Input, Output
+
+
+def test_dev100_prop_check_errors_with_path(dash_duo):
+    app = dash.Dash(__name__)
+
+    test_cases = {
+        "not-boolean": {
+            "fail": True,
+            "name": 'simple "not a boolean" check',
+            "component": dcc.Graph,
+            "props": {"animate": 0},
+        },
+        "missing-required-nested-prop": {
+            "fail": True,
+            "name": 'missing required "value" inside options',
+            "component": dcc.Checklist,
+            "props": {"options": [{"label": "hello"}], "values": ["test"]},
+        },
+        "invalid-nested-prop": {
+            "fail": True,
+            "name": "invalid nested prop",
+            "component": dcc.Checklist,
+            "props": {
+                "options": [{"label": "hello", "value": True}],
+                "values": ["test"],
+            },
+        },
+        "invalid-arrayOf": {
+            "fail": True,
+            "name": "invalid arrayOf",
+            "component": dcc.Checklist,
+            "props": {"options": "test", "values": []},
+        },
+        "invalid-oneOf": {
+            "fail": True,
+            "name": "invalid oneOf",
+            "component": dcc.Input,
+            "props": {"type": "test"},
+        },
+        "invalid-oneOfType": {
+            "fail": True,
+            "name": "invalid oneOfType",
+            "component": dcc.Input,
+            "props": {"max": True},
+        },
+        "invalid-shape-1": {
+            "fail": True,
+            "name": "invalid key within nested object",
+            "component": dcc.Graph,
+            "props": {"config": {"asdf": "that"}},
+        },
+        "invalid-shape-2": {
+            "fail": True,
+            "name": "nested object with bad value",
+            "component": dcc.Graph,
+            "props": {"config": {"edits": {"legendPosition": "asdf"}}},
+        },
+        "invalid-shape-3": {
+            "fail": True,
+            "name": "invalid oneOf within nested object",
+            "component": dcc.Graph,
+            "props": {"config": {"toImageButtonOptions": {"format": "asdf"}}},
+        },
+        "invalid-shape-4": {
+            "fail": True,
+            "name": "invalid key within deeply nested object",
+            "component": dcc.Graph,
+            "props": {"config": {"toImageButtonOptions": {"asdf": "test"}}},
+        },
+        "invalid-shape-5": {
+            "fail": True,
+            "name": "invalid not required key",
+            "component": dcc.Dropdown,
+            "props": {
+                "options": [
+                    {"label": "new york", "value": "ny", "typo": "asdf"}
+                ]
+            },
+        },
+        "string-not-list": {
+            "fail": True,
+            "name": "string-not-a-list",
+            "component": dcc.Checklist,
+            "props": {
+                "options": [{"label": "hello", "value": "test"}],
+                "values": "test",
+            },
+        },
+        "no-properties": {
+            "fail": False,
+            "name": "no properties",
+            "component": dcc.Graph,
+            "props": {},
+        },
+        "nested-children": {
+            "fail": True,
+            "name": "nested children",
+            "component": html.Div,
+            "props": {"children": [[1]]},
+        },
+        "deeply-nested-children": {
+            "fail": True,
+            "name": "deeply nested children",
+            "component": html.Div,
+            "props": {"children": html.Div([html.Div([3, html.Div([[10]])])])},
+        },
+        "dict": {
+            "fail": True,
+            "name": "returning a dictionary",
+            "component": html.Div,
+            "props": {"children": {"hello": "world"}},
+        },
+        "nested-prop-failure": {
+            "fail": True,
+            "name": "nested string instead of number/null",
+            "component": dcc.Graph,
+            "props": {
+                "figure": {"data": [{}]},
+                "config": {
+                    "toImageButtonOptions": {"width": None, "height": "test"}
+                },
+            },
+        },
+        "allow-null": {
+            "fail": False,
+            "name": "nested null",
+            "component": dcc.Graph,
+            "props": {
+                "figure": {"data": [{}]},
+                "config": {
+                    "toImageButtonOptions": {"width": None, "height": None}
+                },
+            },
+        },
+        "allow-null-2": {
+            "fail": False,
+            "name": "allow null as value",
+            "component": dcc.Dropdown,
+            "props": {"value": None},
+        },
+        "allow-null-3": {
+            "fail": False,
+            "name": "allow null in properties",
+            "component": dcc.Input,
+            "props": {"value": None},
+        },
+        "allow-null-4": {
+            "fail": False,
+            "name": "allow null in oneOfType",
+            "component": dcc.Store,
+            "props": {"id": "store", "data": None},
+        },
+        "long-property-string": {
+            "fail": True,
+            "name": "long property string with id",
+            "component": html.Div,
+            "props": {"id": "pink div", "style": "color: hotpink; " * 1000},
+        },
+        "multiple-wrong-values": {
+            "fail": True,
+            "name": "multiple wrong props",
+            "component": dcc.Dropdown,
+            "props": {"id": "dropdown", "value": 10, "options": "asdf"},
+        },
+        "boolean-html-properties": {
+            "fail": True,
+            "name": "dont allow booleans for dom props",
+            "component": html.Div,
+            "props": {"contentEditable": True},
+        },
+        "allow-exact-with-optional-and-required-1": {
+            "fail": False,
+            "name": "allow exact with optional and required keys",
+            "component": dcc.Dropdown,
+            "props": {
+                "options": [
+                    {"label": "new york", "value": "ny", "disabled": False}
+                ]
+            },
+        },
+        "allow-exact-with-optional-and-required-2": {
+            "fail": False,
+            "name": "allow exact with optional and required keys 2",
+            "component": dcc.Dropdown,
+            "props": {"options": [{"label": "new york", "value": "ny"}]},
+        },
+    }
+
+    app.layout = html.Div(
+        [html.Div(id="content"), dcc.Location(id="location")])
+
+    @app.callback(
+        Output("content", "children"), [Input("location", "pathname")]
+    )
+    def display_content(pathname):
+        if pathname is None or pathname == "/":
+            return "Initial state"
+        test_case = test_cases[pathname.strip("/")]
+        return html.Div(
+            id="new-component",
+            children=test_case["component"](**test_case["props"]),
+        )
+
+    dash_duo.start_app_server(
+        app,
+        debug=True,
+        use_reloader=False,
+        use_debugger=True,
+        dev_tools_hot_reload=False,
+    )
+
+    for tcid in test_cases:
+        dash_duo.driver.get("{}/{}".format(dash_duo.server_url, tcid))
+        if test_cases[tcid]["fail"]:
+            try:
+                dash_duo.wait_for_element(
+                    ".test-devtools-error-toggle"
+                ).click()
+            except Exception as e:
+                raise Exception(
+                    "Error popup not shown for {}".format(tcid)
+                )
+            dash_duo.percy_snapshot(
+                "devtools validation exception: {}".format(
+                    test_cases[tcid]["name"]
+                )
+            )
+        else:
+            try:
+                dash_duo.wait_for_element("#new-component")
+            except Exception as e:
+                raise Exception(
+                    "Component not rendered in {}".format(tcid)
+                )
+            dash_duo.percy_snapshot(
+                "devtools validation no exception: {}".format(
+                    test_cases[tcid]["name"]
+                )
+            )
