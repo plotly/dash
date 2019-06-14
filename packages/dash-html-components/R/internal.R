2fe69d03944198c04cc93dcba95210223320ab1b
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2019-06-14 01:02:25 -0400

    bug: missing quotation marks

--- packages/dash-html-components/R/internal.R
+++ packages/dash-html-components/R/internal.R
@@ -2,7 +2,7 @@
 deps_metadata <- list(`dash_html_components` = structure(list(name = "dash_html_components",
 version = "0.16.0", src = list(href = NULL,
 file = "deps"), meta = NULL,
-script = dash_html_components.min.js,
+script = "dash_html_components.min.js",
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashHtmlComponents",
 all_files = FALSE), class = "html_dependency"))
 return(deps_metadata)
