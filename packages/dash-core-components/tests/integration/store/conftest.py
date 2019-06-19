eb6c0252c194afecf754cc9b99789503ff809553
byron <byron@plot.ly>
2019-06-18 23:38:57 -0400

    white_check_mark: add new test for data lifecycle

---
+++ packages/dash-core-components/tests/integration/store/conftest.py
@@ -0,0 +1,61 @@
+import json
+import pytest
+import uuid
+import dash
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_core_components as dcc
+import dash_html_components as html
+
+UUID = "store-test-{}".format(uuid.uuid4().hex)
+
+@pytest.fixture(scope="module")
+def store_app():
+    app = dash.Dash(__name__)
+    app.uuid = UUID
+    app.layout = html.Div(
+        [
+            dcc.Store(id="memory", storage_type="memory", data=app.uuid),
+            dcc.Store(id="local", storage_type="local"),
+            dcc.Store(id="session", storage_type="session"),
+            html.Button("click me", id="btn"),
+            html.Button("clear data", id="clear-btn"),
+            html.Div(id="output"),
+        ]
+    )
+
+    @app.callback(
+        Output("output", "children"),
+        [Input("memory", "modified_timestamp")],
+        [State("memory", "data")],
+    )
+    def write_memory(modified_ts, data):
+        return json.dumps(data)
+
+    @app.callback(
+        [
+            Output("local", "clear_data"),
+            Output("memory", "clear_data"),
+            Output("session", "clear_data"),
+        ],
+        [Input("clear-btn", "n_clicks")],
+    )
+    def on_clear(n_clicks):
+        if n_clicks is None:
+            raise PreventUpdate
+        return True
+
+    @app.callback(
+        [
+            Output("memory", "data"),
+            Output("local", "data"),
+            Output("session", "data"),
+        ],
+        [Input("btn", "n_clicks")],
+    )
+    def on_click(n_clicks):
+        if n_clicks is None:
+            raise PreventUpdate
+        return ({"n_clicks": n_clicks},) * 3
+
+    yield app
---
+++ packages/dash-core-components/tests/integration/store/test_data_lifecycle.py
@@ -0,0 +1,47 @@
+def test_stdl001_data_lifecycle_with_different_condition(store_app, dash_duo):
+    dash_duo.start_server(store_app)
+
+    nclicks = 10
+    dash_duo.multiple_click("#btn", nclicks)
+
+    dash_duo.wait_for_text_to_equal(
+        "#output", '{{"n_clicks": {}}}'.format(nclicks)
+    )
+    assert dash_duo.local_storage == {
+        "n_clicks": nclicks
+    }, "local storage should contain the same click nums"
+    assert dash_duo.session_storage == {
+        "n_clicks": nclicks
+    }, "session storage should contain the same click nums"
+
+    dash_duo.driver.refresh()
+    assert dash_duo.find_element("#output").text == '"{}"'.format(
+        store_app.uuid
+    ), "a browser refresh will clear the memory type data to initial data"
+    assert dash_duo.local_storage == {"n_clicks": nclicks}
+    assert dash_duo.session_storage == {"n_clicks": nclicks}
+
+    dash_duo.open_new_tab()
+    dash_duo.toggle_window()  # switch to the new tab
+    assert dash_duo.local_storage == {
+        "n_clicks": nclicks
+    }, "local storage should be persistent"
+    assert dash_duo.find_element("#output").text == '"{}"'.format(
+        store_app.uuid
+    ), "memory storage should contain the initial data in new tab"
+
+    dash_duo.multiple_click("#btn", 2)
+    assert dash_duo.session_storage == {"n_clicks": 2}
+    assert (
+        '"n_clicks": 2' in dash_duo.wait_for_element("#output").text
+    ), "memory storage should reflect to the new clicks"
+
+    dash_duo.driver.close()
+    dash_duo.switch_window()
+    assert dash_duo.local_storage == {"n_clicks": 2}
+    assert dash_duo.find_element("#output").text == '"{}"'.format(
+        store_app.uuid
+    ), "memory output should be the same as after previous refresh"
+    assert dash_duo.session_storage == {
+        "n_clicks": nclicks
+    }, "session storage should be specific per browser tab window"
