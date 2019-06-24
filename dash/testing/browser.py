7727fd1fe25467809ee2818698f79dfc1127725e
TakaakiFuruse <takaakifuruse@gmail.com>
2019-06-24 15:34:09 +0900

    Added send key input function to dash.testing
    **Problem**
    Currently, dash.testing has [clear_input](https://github.com/plotly/dash/blob/149a8e853f7c94df55cd75c1735bec97717ae746/dash/testing/browser.py#L292) function which does
    1. Click input box
    2. Clear text
    
    But, there's no function to input text to an input.
    Adding such function would increase uability of dash.testing.
    
    **Solution**
    
    I have added a function to `dash/dash/testing/browser.py`
    
    **Additional context**
    
    I know that we can do the same thing like...
    
    ```py
        input1 = dash_duo.find_element('#input')
        dash_duo.clear_input(input1)
        input1.send_keys('hello world')
    ```
    
    It's working, but seems a bit cumbersome to me.
    (Documenting the code would be lengthy too.)

--- dash/testing/browser.py
+++ dash/testing/browser.py
@@ -301,6 +301,14 @@ class Browser(DashPageMixin):
             .send_keys(Keys.DELETE)
         ).perform()
 
+    def send_key_input(self, elem, key_input):
+        """send key input to an input"""
+        (
+            ActionChains(self.driver)
+            .click(elem)
+            .send_keys(key_input)
+        ).perform()
+
     def get_logs(self):
         """return a list of `SEVERE` level logs after last reset time stamps
         (default to 0, resettable by `reset_log_timestamp`. Chrome only
