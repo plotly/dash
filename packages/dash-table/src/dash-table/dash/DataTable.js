cd28451d75d80abf459acf2e90c264b10a3b5bc1
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2019-07-02 14:50:34 -0400

    Build Dash 4.0.0 version of dash-table for R (#478)

--- packages/dash-table/src/dash-table/dash/DataTable.js
+++ packages/dash-table/src/dash-table/dash/DataTable.js
@@ -10,6 +10,15 @@ import genRandomId from 'dash-table/utils/generate';
 import isValidProps from './validate';
 import Sanitizer from './Sanitizer';
 
+/**
+ * Dash DataTable is an interactive table component designed for 
+ * designed for viewing, editing, and exploring large datasets.
+ * DataTable is rendered with standard, semantic HTML <table/> markup,
+ * which makes it accessible, responsive, and easy to style. This
+ * component was written from scratch in React.js specifically for the 
+ * Dash community. Its API was designed to be ergonomic and its behavior
+ * is completely customizable through its properties.
+ */
 export default class DataTable extends Component {
     constructor(props) {
         super(props);
