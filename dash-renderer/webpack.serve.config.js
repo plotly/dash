ba8d5ba7cbc3215cca6431aace67c805418e3a87
alexcjohnson <alex@plot.ly>
2019-09-12 22:16:04 -0400

    update build to corejs@3

--- dash-renderer/webpack.serve.config.js
+++ dash-renderer/webpack.serve.config.js
@@ -3,6 +3,6 @@ const config = require('./webpack.config.js');
 config.entry = {main: './src/demo/index.js'};
 config.output = {filename: 'output.js'};
 config.mode = 'development';
-config.externals = undefined; // eslint-disable-line
+config.externals = undefined;
 config.devtool = 'inline-source-map';
 module.exports = config;
