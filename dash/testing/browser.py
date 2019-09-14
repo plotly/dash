3fc2894961b6b5e7ae87e1c9142abd06c9fbbc82
byron <byron@plot.ly>
2019-09-13 20:43:04 -0400

    black and add log

--- dash/testing/browser.py
+++ dash/testing/browser.py
@@ -39,7 +39,7 @@ class Browser(DashPageMixin):
         options=None,
         download_path=None,
         percy_finalize=True,
-        percy_assets_root='tests/assets',
+        percy_assets_root="tests/assets",
         wait_timeout=10,
     ):
         self._browser = browser.lower()
@@ -76,6 +76,9 @@ class Browser(DashPageMixin):
         logger.debug("initialize browser with arguments")
         logger.debug("  headless => %s", self._headless)
         logger.debug("  download_path => %s", self._download_path)
+        logger.debug(
+            "  percy asset root => %s", os.path.abspath(percy_assets_root)
+        )
 
     def __enter__(self):
         return self
