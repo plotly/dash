4e787b6d6a04196e9d84d9c3dd7b359713d9758c
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-09-27 10:08:32 -0400

    lint

--- dash/resources.py
+++ dash/resources.py
@@ -18,10 +18,16 @@ class Resources:
     def _filter_resources(self, all_resources, dev_bundles=False):
         filtered_resources = []
         for s in all_resources:
-            f = {}
+            filtered_resource = {}
             if 'dynamic' in s:
-                f['dynamic'] = s['dynamic']
+                filtered_resource['dynamic'] = s['dynamic']
             if 'async' in s:
+                if 'dynamic' in s:
+                    raise exceptions.ResourceException(
+                        '{} can\'t have both \'dynamic\' and '
+                        '\'async\'.'.format(json.dumps(filtered_resource))
+                    )
+
                 # Async assigns a value dynamically to 'dynamic'
                 # based on the value of 'async' and config.eager_loading
                 #
@@ -30,41 +36,41 @@ class Resources:
                 # 'eager' -> dynamic if server is not eager
                 # (to prevent ever loading it)
                 if s['async'] is True:
-                    f['dynamic'] = not self.config.eager_loading
+                    filtered_resource['dynamic'] = not self.config.eager_loading
 
-                if (
-                    (s['async'] == 'eager' and self.config.eager_loading) or
-                    s['async'] == 'lazy'
-                ):
-                    f['dynamic'] = True
+                if (s['async'] == 'eager' and self.config.eager_loading) or s[
+                    'async'
+                ] == 'lazy':
+                    filtered_resource['dynamic'] = True
                 else:
-                    f['dynamic'] = False
+                    filtered_resource['dynamic'] = False
             if 'namespace' in s:
-                f['namespace'] = s['namespace']
+                filtered_resource['namespace'] = s['namespace']
             if 'external_url' in s and not self.config.serve_locally:
-                f['external_url'] = s['external_url']
+                filtered_resource['external_url'] = s['external_url']
             elif 'dev_package_path' in s and dev_bundles:
-                f['relative_package_path'] = (
-                    s['dev_package_path']
-                )
+                filtered_resource['relative_package_path'] = s[
+                    'dev_package_path'
+                ]
             elif 'relative_package_path' in s:
-                f['relative_package_path'] = (
-                    s['relative_package_path']
-                )
+                filtered_resource['relative_package_path'] = s[
+                    'relative_package_path'
+                ]
             elif 'absolute_path' in s:
-                f['absolute_path'] = s['absolute_path']
+                filtered_resource['absolute_path'] = s['absolute_path']
             elif 'asset_path' in s:
                 info = os.stat(s['filepath'])
-                f['asset_path'] = s['asset_path']
-                f['ts'] = info.st_mtime
+                filtered_resource['asset_path'] = s['asset_path']
+                filtered_resource['ts'] = info.st_mtime
             elif self.config.serve_locally:
-                warnings.warn((
-                    'You have set your config to `serve_locally=True` but '
-                    'A local version of {} is not available.\n'
-                    'If you added this file with `app.scripts.append_script` '
-                    'or `app.css.append_css`, use `external_scripts` '
-                    'or `external_stylesheets` instead.\n'
-                    'See https://dash.plot.ly/external-resources'
+                warnings.warn(
+                    (
+                        'You have set your config to `serve_locally=True` but '
+                        'A local version of {} is not available.\n'
+                        'If you added this file with `app.scripts.append_script` '
+                        'or `app.css.append_css`, use `external_scripts` '
+                        'or `external_stylesheets` instead.\n'
+                        'See https://dash.plot.ly/external-resources'
                     ).format(s['external_url'])
                 )
                 continue
@@ -72,12 +78,10 @@ class Resources:
                 raise exceptions.ResourceException(
                     '{} does not have a '
                     'relative_package_path, absolute_path, or an '
-                    'external_url.'.format(
-                        json.dumps(f)
-                    )
+                    'external_url.'.format(json.dumps(filtered_resource))
                 )
 
-            filtered_resources.append(f)
+            filtered_resources.append(filtered_resource)
 
         return filtered_resources
 
