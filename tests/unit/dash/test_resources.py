a598dae8bba3714f32265d81c560701b8d822b65
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-10-10 15:49:01 -0400

    black

--- tests/unit/dash/test_resources.py
+++ tests/unit/dash/test_resources.py
@@ -1,59 +1,54 @@
 from dash.resources import Resources, Scripts
 
+
 class obj(object):
     def __init__(self, dict):
         self.__dict__ = dict
 
+
 def test_resources_eager():
 
-    resource = Resources('js_test')
-    resource.config = obj({
-        'eager_loading': True,
-        'serve_locally': False
-    })
+    resource = Resources("js_test")
+    resource.config = obj({"eager_loading": True, "serve_locally": False})
 
-    filtered = resource._filter_resources([{
-        'async': 'eager',
-        'external_url': 'a.js'
-    }, {
-        'async': 'lazy',
-        'external_url': 'b.js'
-    }, {
-        'async': True,
-        'external_url': 'c.js'
-    }], False)
+    filtered = resource._filter_resources(
+        [
+            {"async": "eager", "external_url": "a.js"},
+            {"async": "lazy", "external_url": "b.js"},
+            {"async": True, "external_url": "c.js"},
+        ],
+        False,
+    )
 
-    assert(len(filtered)==3)
-    assert(filtered[0].get('external_url') == 'a.js')
-    assert(filtered[0].get('dynamic') == False) # include (eager when eager)
-    assert(filtered[1].get('external_url') == 'b.js')
-    assert(filtered[1].get('dynamic') == True) # exclude (lazy when eager -> closest to exclude)
-    assert(filtered[2].get('external_url') == 'c.js')
-    assert(filtered[2].get('dynamic') == False) # include (always matches settings)
+    assert len(filtered) == 3
+    assert filtered[0].get("external_url") == "a.js"
+    assert filtered[0].get("dynamic") == False  # include (eager when eager)
+    assert filtered[1].get("external_url") == "b.js"
+    assert (
+        filtered[1].get("dynamic") == True
+    )  # exclude (lazy when eager -> closest to exclude)
+    assert filtered[2].get("external_url") == "c.js"
+    assert filtered[2].get("dynamic") == False  # include (always matches settings)
+
 
 def test_resources_lazy():
 
-    resource = Resources('js_test')
-    resource.config = obj({
-        'eager_loading': False,
-        'serve_locally': False
-    })
+    resource = Resources("js_test")
+    resource.config = obj({"eager_loading": False, "serve_locally": False})
 
-    filtered = resource._filter_resources([{
-        'async': 'eager',
-        'external_url': 'a.js'
-    }, {
-        'async': 'lazy',
-        'external_url': 'b.js'
-    }, {
-        'async': True,
-        'external_url': 'c.js'
-    }], False)
+    filtered = resource._filter_resources(
+        [
+            {"async": "eager", "external_url": "a.js"},
+            {"async": "lazy", "external_url": "b.js"},
+            {"async": True, "external_url": "c.js"},
+        ],
+        False,
+    )
 
-    assert(len(filtered)==3)
-    assert(filtered[0].get('external_url') == 'a.js')
-    assert(filtered[0].get('dynamic') == True) # exclude (no eager when lazy)
-    assert(filtered[1].get('external_url') == 'b.js')
-    assert(filtered[1].get('dynamic') == True) # exclude (lazy when lazy)
-    assert(filtered[2].get('external_url') == 'c.js')
-    assert(filtered[2].get('dynamic') == True) # exclude (always matches settings)
+    assert len(filtered) == 3
+    assert filtered[0].get("external_url") == "a.js"
+    assert filtered[0].get("dynamic") == True  # exclude (no eager when lazy)
+    assert filtered[1].get("external_url") == "b.js"
+    assert filtered[1].get("dynamic") == True  # exclude (lazy when lazy)
+    assert filtered[2].get("external_url") == "c.js"
+    assert filtered[2].get("dynamic") == True  # exclude (always matches settings)
