284bcd8a9a41eee3b025923471de1022c31c2db1
alexcjohnson <alex@plot.ly>
2019-10-16 16:08:55 -0400

    clean up react devtools warnings on our devtools and remove partially-implemented resolveError, which was making everything much more complicated, especially when I tried to clean up ComponentErrorBoundary

--- dash-renderer/src/components/error/GlobalErrorContainer.react.js
+++ dash-renderer/src/components/error/GlobalErrorContainer.react.js
@@ -2,7 +2,6 @@ import {connect} from 'react-redux';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Radium from 'radium';
-import {resolveError} from '../../actions';
 import {DebugMenu} from './menu/DebugMenu.react';
 
 class UnconnectedGlobalErrorContainer extends Component {
@@ -10,24 +9,13 @@ class UnconnectedGlobalErrorContainer extends Component {
         super(props);
     }
 
-    resolveError(dispatch, type, myId) {
-        if (type === 'backEnd') {
-            dispatch(resolveError({type}));
-            // dispatch(revert);
-        } else {
-            dispatch(resolveError({myId, type}));
-        }
-    }
-
     render() {
-        const {error, dispatch, dependenciesRequest} = this.props;
+        const {error, dependenciesRequest} = this.props;
         return (
             <div id="_dash-global-error-container">
                 <DebugMenu
                     error={error}
                     dependenciesRequest={dependenciesRequest}
-                    dispatch={dispatch}
-                    resolveError={this.resolveError}
                 >
                     <div id="_dash-app-content">{this.props.children}</div>
                 </DebugMenu>
@@ -40,15 +28,11 @@ UnconnectedGlobalErrorContainer.propTypes = {
     children: PropTypes.object,
     error: PropTypes.object,
     dependenciesRequest: PropTypes.object,
-    dispatch: PropTypes.func,
 };
 
-const GlobalErrorContainer = connect(
-    state => ({
-        error: state.error,
-        dependenciesRequest: state.dependenciesRequest,
-    }),
-    dispatch => ({dispatch})
-)(Radium(UnconnectedGlobalErrorContainer));
+const GlobalErrorContainer = connect(state => ({
+    error: state.error,
+    dependenciesRequest: state.dependenciesRequest,
+}))(Radium(UnconnectedGlobalErrorContainer));
 
 export default GlobalErrorContainer;
