f156edeaf6641539465464801bd46b1b308a68dc
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-10-28 13:11:21 -0400

    plugin - distinguish local from remote script resources

--- @plotly/webpack-dash-dynamic-import/src/index.js
+++ @plotly/webpack-dash-dynamic-import/src/index.js
@@ -11,15 +11,25 @@ function getFingerprint() {
 }
 
 const resolveImportSource = () => `\
+const getCurrentScript = function() {
+    let script = document.currentScript;
+    if (!script) {
+        /* Shim for IE11 and below */
+        /* Do not take into account async scripts and inline scripts */
+        const scripts = Array.from(document.getElementsByTagName('script')).filter(function(s) { return !s.async && !s.text && !s.textContent; });
+        script = scripts.slice(-1)[0];
+    }
+
+    return script;
+};
+
+const isLocalScript = function(script) {
+    return /\/_dash-components-suite\//.test(script.src);
+};
+
 Object.defineProperty(__webpack_require__, 'p', {
     get: (function () {
-        let script = document.currentScript;
-        if (!script) {
-            /* Shim for IE11 and below */
-            /* Do not take into account async scripts and inline scripts */
-            const scripts = Array.from(document.getElementsByTagName('script')).filter(function(s) { return !s.async && !s.text && !s.textContent; });
-            script = scripts.slice(-1)[0];
-        }
+        let script = getCurrentScript();
 
         var url = script.src.split('/').slice(0, -1).join('/') + '/';
 
@@ -31,14 +41,23 @@ Object.defineProperty(__webpack_require__, 'p', {
 
 const __jsonpScriptSrc__ = jsonpScriptSrc;
 jsonpScriptSrc = function(chunkId) {
-    const srcFragments = __jsonpScriptSrc__(chunkId).split('/');
+    let script = getCurrentScript();
+    let isLocal = isLocalScript(script);
+
+    let src = __jsonpScriptSrc__(chunkId);
+
+    if(!isLocal) {
+        return src;
+    }
+
+    const srcFragments = src.split('/');
     const fileFragments = srcFragments.slice(-1)[0].split('.');
 
     fileFragments.splice(1, 0, ${getFingerprint()});
     srcFragments.splice(-1, 1, fileFragments.join('.'))
 
     return srcFragments.join('/');
-}
+};
 `
 
 class WebpackDashDynamicImport {
