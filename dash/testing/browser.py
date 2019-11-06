c20db88fb4f50839fdfdb806279e6ebb8126c212
byron <byron@plot.ly>
2019-11-06 13:44:57 -0500

    remove pause

--- dash/testing/browser.py
+++ dash/testing/browser.py
@@ -457,7 +457,6 @@ class Browser(DashPageMixin):
         (
             ActionChains(self.driver)
             .move_to_element(elem)
-            .pause(0.2)
             .click(elem)
             .send_keys(Keys.END)
             .key_down(Keys.SHIFT)
