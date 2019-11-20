ce032a3337a3e6a4be49f4f1558b419c6681e734
byron <byron@plot.ly>
2019-11-20 16:54:48 -0500

    the skip approarch in plugin does not work, revert it back

--- dash/testing/plugin.py
+++ dash/testing/plugin.py
@@ -1,157 +1,158 @@
 # pylint: disable=missing-docstring,redefined-outer-name
-import pkgutil
 import pytest
 from .consts import SELENIUM_GRID_DEFAULT
 
-# this is not a complete guess, but picking few typical dash dependencies in
-# require-testing.txt
-if (
-    pkgutil.find_loader("waitress")
-    and pkgutil.find_loader("percy")
-    and pkgutil.find_loader("beautifulsoup4")
-):
-    try:
-        from dash.testing.application_runners import (
-            ThreadedRunner,
-            ProcessRunner,
-            RRunner,
-        )
-        from dash.testing.browser import Browser
-        from dash.testing.composite import DashComposite, DashRComposite
-    except ImportError:
-        pass
 
-    def pytest_addoption(parser):
-        dash = parser.getgroup("Dash", "Dash Integration Tests")
+try:
+    from dash.testing.application_runners import (
+        ThreadedRunner,
+        ProcessRunner,
+        RRunner,
+    )
+    from dash.testing.browser import Browser
+    from dash.testing.composite import DashComposite, DashRComposite
+except ImportError:
+    pass
 
-        dash.addoption(
-            "--webdriver",
-            choices=("Chrome", "Firefox"),
-            default="Chrome",
-            help="Name of the selenium driver to use",
-        )
 
-        dash.addoption(
-            "--remote",
-            action="store_true",
-            help="instruct pytest to use selenium grid",
-        )
+def pytest_addoption(parser):
+    dash = parser.getgroup("Dash", "Dash Integration Tests")
 
-        dash.addoption(
-            "--remote-url",
-            action="store",
-            default=SELENIUM_GRID_DEFAULT,
-            help="set a different selenium grid remote url if other than default",
-        )
+    dash.addoption(
+        "--webdriver",
+        choices=("Chrome", "Firefox"),
+        default="Chrome",
+        help="Name of the selenium driver to use",
+    )
 
-        dash.addoption(
-            "--headless",
-            action="store_true",
-            help="set this flag to run in headless mode",
-        )
+    dash.addoption(
+        "--remote",
+        action="store_true",
+        help="instruct pytest to use selenium grid",
+    )
 
-        dash.addoption(
-            "--percy-assets",
-            action="store",
-            default="tests/assets",
-            help="configure how Percy will discover your app's assets",
-        )
+    dash.addoption(
+        "--remote-url",
+        action="store",
+        default=SELENIUM_GRID_DEFAULT,
+        help="set a different selenium grid remote url if other than default",
+    )
 
-        dash.addoption(
-            "--nopercyfinalize",
-            action="store_false",
-            help="set this flag to control percy finalize at CI level",
-        )
+    dash.addoption(
+        "--headless",
+        action="store_true",
+        help="set this flag to run in headless mode",
+    )
 
-    @pytest.mark.tryfirst
-    def pytest_addhooks(pluginmanager):
-        # https://github.com/pytest-dev/pytest-xdist/blob/974bd566c599dc6a9ea291838c6f226197208b46/xdist/plugin.py#L67
-        # avoid warnings with pytest-2.8
-        from dash.testing import newhooks
+    dash.addoption(
+        "--percy-assets",
+        action="store",
+        default="tests/assets",
+        help="configure how Percy will discover your app's assets",
+    )
 
-        method = getattr(pluginmanager, "add_hookspecs", None)
-        if method is None:
-            method = pluginmanager.addhooks  # pragma: no cover
-        method(newhooks)
+    dash.addoption(
+        "--nopercyfinalize",
+        action="store_false",
+        help="set this flag to control percy finalize at CI level",
+    )
 
-    @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-    def pytest_runtest_makereport(
-        item, call
-    ):  # pylint: disable=unused-argument
-        # execute all other hooks to obtain the report object
-        outcome = yield
-        rep = outcome.get_result()
 
-        # we only look at actual failing test calls, not setup/teardown
-        if rep.when == "call" and rep.failed:
-            for name, fixture in item.funcargs.items():
-                try:
-                    if name in {"dash_duo", "dash_br", "dashr"}:
-                        fixture.take_snapshot(item.name)
-                except Exception as e:  # pylint: disable=broad-except
-                    print(e)
+@pytest.mark.tryfirst
+def pytest_addhooks(pluginmanager):
+    # https://github.com/pytest-dev/pytest-xdist/blob/974bd566c599dc6a9ea291838c6f226197208b46/xdist/plugin.py#L67
+    # avoid warnings with pytest-2.8
+    from dash.testing import newhooks
 
-    ###############################################################################
-    # Fixtures
-    ###############################################################################
+    method = getattr(pluginmanager, "add_hookspecs", None)
+    if method is None:
+        method = pluginmanager.addhooks  # pragma: no cover
+    method(newhooks)
 
-    @pytest.fixture
-    def dash_thread_server():
-        """Start a local dash server in a new thread."""
-        with ThreadedRunner() as starter:
-            yield starter
 
-    @pytest.fixture
-    def dash_process_server():
-        """Start a Dash server with subprocess.Popen and waitress-serve."""
-        with ProcessRunner() as starter:
-            yield starter
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):  # pylint: disable=unused-argument
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
 
-    @pytest.fixture
-    def dashr_server():
-        with RRunner() as starter:
-            yield starter
+    # we only look at actual failing test calls, not setup/teardown
+    if rep.when == "call" and rep.failed:
+        for name, fixture in item.funcargs.items():
+            try:
+                if name in {"dash_duo", "dash_br", "dashr"}:
+                    fixture.take_snapshot(item.name)
+            except Exception as e:  # pylint: disable=broad-except
+                print(e)
 
-    @pytest.fixture
-    def dash_br(request, tmpdir):
-        with Browser(
-            browser=request.config.getoption("webdriver"),
-            remote=request.config.getoption("remote"),
-            remote_url=request.config.getoption("remote_url"),
-            headless=request.config.getoption("headless"),
-            options=request.config.hook.pytest_setup_options(),
-            download_path=tmpdir.mkdir("download").strpath,
-            percy_assets_root=request.config.getoption("percy_assets"),
-            percy_finalize=request.config.getoption("nopercyfinalize"),
-        ) as browser:
-            yield browser
 
-    @pytest.fixture
-    def dash_duo(request, dash_thread_server, tmpdir):
-        with DashComposite(
-            dash_thread_server,
-            browser=request.config.getoption("webdriver"),
-            remote=request.config.getoption("remote"),
-            remote_url=request.config.getoption("remote_url"),
-            headless=request.config.getoption("headless"),
-            options=request.config.hook.pytest_setup_options(),
-            download_path=tmpdir.mkdir("download").strpath,
-            percy_assets_root=request.config.getoption("percy_assets"),
-            percy_finalize=request.config.getoption("nopercyfinalize"),
-        ) as dc:
-            yield dc
+###############################################################################
+# Fixtures
+###############################################################################
 
-    @pytest.fixture
-    def dashr(request, dashr_server, tmpdir):
-        with DashRComposite(
-            dashr_server,
-            browser=request.config.getoption("webdriver"),
-            remote=request.config.getoption("remote"),
-            remote_url=request.config.getoption("remote_url"),
-            headless=request.config.getoption("headless"),
-            options=request.config.hook.pytest_setup_options(),
-            download_path=tmpdir.mkdir("download").strpath,
-            percy_assets_root=request.config.getoption("percy_assets"),
-            percy_finalize=request.config.getoption("nopercyfinalize"),
-        ) as dc:
-            yield dc
+
+@pytest.fixture
+def dash_thread_server():
+    """Start a local dash server in a new thread."""
+    with ThreadedRunner() as starter:
+        yield starter
+
+
+@pytest.fixture
+def dash_process_server():
+    """Start a Dash server with subprocess.Popen and waitress-serve."""
+    with ProcessRunner() as starter:
+        yield starter
+
+
+@pytest.fixture
+def dashr_server():
+    with RRunner() as starter:
+        yield starter
+
+
+@pytest.fixture
+def dash_br(request, tmpdir):
+    with Browser(
+        browser=request.config.getoption("webdriver"),
+        remote=request.config.getoption("remote"),
+        remote_url=request.config.getoption("remote_url"),
+        headless=request.config.getoption("headless"),
+        options=request.config.hook.pytest_setup_options(),
+        download_path=tmpdir.mkdir("download").strpath,
+        percy_assets_root=request.config.getoption("percy_assets"),
+        percy_finalize=request.config.getoption("nopercyfinalize"),
+    ) as browser:
+        yield browser
+
+
+@pytest.fixture
+def dash_duo(request, dash_thread_server, tmpdir):
+    with DashComposite(
+        dash_thread_server,
+        browser=request.config.getoption("webdriver"),
+        remote=request.config.getoption("remote"),
+        remote_url=request.config.getoption("remote_url"),
+        headless=request.config.getoption("headless"),
+        options=request.config.hook.pytest_setup_options(),
+        download_path=tmpdir.mkdir("download").strpath,
+        percy_assets_root=request.config.getoption("percy_assets"),
+        percy_finalize=request.config.getoption("nopercyfinalize"),
+    ) as dc:
+        yield dc
+
+
+@pytest.fixture
+def dashr(request, dashr_server, tmpdir):
+    with DashRComposite(
+        dashr_server,
+        browser=request.config.getoption("webdriver"),
+        remote=request.config.getoption("remote"),
+        remote_url=request.config.getoption("remote_url"),
+        headless=request.config.getoption("headless"),
+        options=request.config.hook.pytest_setup_options(),
+        download_path=tmpdir.mkdir("download").strpath,
+        percy_assets_root=request.config.getoption("percy_assets"),
+        percy_finalize=request.config.getoption("nopercyfinalize"),
+    ) as dc:
+        yield dc
