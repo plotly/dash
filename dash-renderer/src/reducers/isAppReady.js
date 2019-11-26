b232b7a14a61b4bde2a070b9941443bf8c740270
Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>
2019-11-26 16:50:23 -0500

    Issue 1010 - Non-rendered async components using `asyncDecorator` lock renderer callbacks (#1027)

--- dash-renderer/src/reducers/isAppReady.js
+++
@@ -1,8 +0,0 @@
-import {getAction} from '../actions/constants';
-
-export default function config(state = false, action) {
-    if (action.type === getAction('SET_APP_READY')) {
-        return action.payload;
-    }
-    return state;
-}
---
+++ dash-renderer/tests/isAppReady.test.js
@@ -0,0 +1,50 @@
+import isAppReady from "../src/actions/isAppReady";
+
+const WAIT = 1000;
+
+describe('isAppReady', () => {
+    let resolve;
+    beforeEach(() => {
+        const promise = new Promise(r => {
+            resolve = r;
+        });
+
+        window.__components = {
+            a: { _dashprivate_isLazyComponentReady: promise },
+            b: {}
+        };
+    });
+
+    it('executes if app is ready', async () => {
+        let done = false;
+        Promise.resolve(isAppReady(
+            [{ namespace: '__components', type: 'b', props: { id: 'comp1' } }],
+            { comp1: [0] },
+            ['comp1']
+        )).then(() => {
+            done = true
+        });
+
+        await new Promise(r => setTimeout(r, WAIT));
+        expect(done).toEqual(true);
+    });
+
+    it('waits on app to be ready', async () => {
+        let done = false;
+        Promise.resolve(isAppReady(
+            [{ namespace: '__components', type: 'a', props: { id: 'comp1' } }],
+            { comp1: [0] },
+            ['comp1']
+        )).then(() => {
+            done = true
+        });
+
+        await new Promise(r => setTimeout(r, WAIT));
+        expect(done).toEqual(false);
+
+        resolve();
+
+        await new Promise(r => setTimeout(r, WAIT));
+        expect(done).toEqual(true);
+    });
+});
