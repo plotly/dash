c4472b0434a66b932f1a0ad77a7e0c25a5cf5741
Ryan Patrick Kyle <ryan@plot.ly>
2020-04-09 14:58:40 -0400

    :sparkles: support for non-CSS/JS deps in assets

--- dash/development/_r_components_generation.py
+++ dash/development/_r_components_generation.py
@@ -45,14 +45,18 @@ frame_element_template = """`{dep_name}` = structure(list(name = "{dep_name}",
 version = "{project_ver}", src = list(href = NULL,
 file = "deps"), meta = NULL,
 script = {script_name},
-stylesheet = {css_name}, head = NULL, attachment = NULL, package = "{rpkgname}",
+stylesheet = {css_name},
+other = {other_name},
+head = NULL, attachment = NULL, package = "{rpkgname}",
 all_files = FALSE{async_or_dynamic}), class = "html_dependency")"""  # noqa:E501
 
 frame_body_template = """`{project_shortname}` = structure(list(name = "{project_shortname}",
 version = "{project_ver}", src = list(href = NULL,
 file = "deps"), meta = NULL,
 script = {script_name},
-stylesheet = {css_name}, head = NULL, attachment = NULL, package = "{rpkgname}",
+stylesheet = {css_name},
+other = {other_name},
+head = NULL, attachment = NULL, package = "{rpkgname}",
 all_files = FALSE{async_or_dynamic}), class = "html_dependency")"""  # noqa:E501
 
 frame_close_template = """)
@@ -258,7 +262,8 @@ def generate_js_metadata(pkg_data, project_shortname):
     sys.path.insert(0, os.getcwd())
     mod = importlib.import_module(project_shortname)
 
-    alldist = getattr(mod, "_js_dist", []) + getattr(mod, "_css_dist", [])
+    alldist = getattr(mod, "_js_dist", []) + getattr(mod, "_css_dist", []) \
+        + getattr(mod, "_asset_dist", [])
 
     project_ver = pkg_data.get("version")
 
@@ -290,9 +295,15 @@ def generate_js_metadata(pkg_data, project_shortname):
             if "css" in rpp:
                 css_name = "'{}'".format(rpp)
                 script_name = "NULL"
-            else:
+                other_name = "NULL"
+            elif "js" in rpp:
                 script_name = "'{}'".format(rpp)
                 css_name = "NULL"
+                other_name = "NULL"
+            else:
+                css_name = "NULL"
+                script_name = "NULL"
+                other_name = "'{}'".format(rpp)
 
             function_frame += [
                 frame_element_template.format(
@@ -302,6 +313,7 @@ def generate_js_metadata(pkg_data, project_shortname):
                     project_shortname=project_shortname,
                     script_name=script_name,
                     css_name=css_name,
+                    other_name=other_name,
                     async_or_dynamic=async_or_dynamic,
                 )
             ]
@@ -315,9 +327,15 @@ def generate_js_metadata(pkg_data, project_shortname):
         if "css" in rpp:
             css_name = "'{}'".format(rpp)
             script_name = "NULL"
-        else:
+            other_name = "NULL"
+        elif "js" in rpp:
             script_name = "'{}'".format(rpp)
             css_name = "NULL"
+            other_name = "NULL"
+        else:
+            script_name = "NULL"
+            css_name = "NULL"
+            other_name = "'{}'".format(rpp)
 
         function_frame_body = frame_body_template.format(
             project_shortname=project_shortname,
@@ -325,6 +343,7 @@ def generate_js_metadata(pkg_data, project_shortname):
             rpkgname=rpkgname,
             script_name=script_name,
             css_name=css_name,
+            other_name=other_name,
             async_or_dynamic=async_or_dynamic,
         )
 
@@ -380,7 +399,7 @@ def write_help_file(name, props, description, prefix, rpkg_data):
 
     default_argtext = ""
     item_text = ""
-    
+
     prop_keys = list(props.keys())
     prop_keys_wc = list(props.keys())
 
