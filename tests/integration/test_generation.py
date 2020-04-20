000c9e3691d7207f94e3a835e8673389c7f0b6b5
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-04-20 12:56:08 -0400

    integration test for standard and nested components

---
+++ tests/integration/test_generation.py
@@ -0,0 +1,21 @@
+from dash_generator_test_component_nested import MyComponent as MyNestedComponent
+from dash_generator_test_component_standard import MyComponent as MyStandardComponent
+from dash_html_components import Div
+
+import dash
+
+
+def test_gene001_simple_callback(dash_duo):
+    app = dash.Dash(__name__)
+
+    app.layout = Div(
+        [
+            MyStandardComponent(id="standard", value="Standard"),
+            MyNestedComponent(id="nested", value="Nested"),
+        ]
+    )
+
+    dash_duo.start_server(app)
+
+    assert dash_duo.wait_for_element("#standard").text == "Standard"
+    assert dash_duo.wait_for_element("#nested").text == "Nested"
