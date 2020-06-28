28630d9c22c6dfa7ac5e865a4ee725e9abfe95ee
Stefan Lehmann <stlm@posteo.de>
2020-06-28 15:51:21 +0200

    Use update_title in DocumentTitle component

--- dash-renderer/src/components/core/DocumentTitle.react.js
+++ dash-renderer/src/components/core/DocumentTitle.react.js
@@ -5,14 +5,16 @@ import PropTypes from 'prop-types';
 class DocumentTitle extends Component {
     constructor(props) {
         super(props);
+        const {update_title} = props.config;
         this.state = {
             initialTitle: document.title,
+            update_title: update_title,
         };
     }
 
     UNSAFE_componentWillReceiveProps(props) {
         if (props.pendingCallbacks.length) {
-            document.title = 'Updating...';
+            document.title = this.state.update_title;
         } else {
             document.title = this.state.initialTitle;
         }
@@ -29,8 +31,10 @@ class DocumentTitle extends Component {
 
 DocumentTitle.propTypes = {
     pendingCallbacks: PropTypes.array.isRequired,
+    update_title: PropTypes.string,
 };
 
 export default connect(state => ({
+    config: state.config,
     pendingCallbacks: state.pendingCallbacks,
 }))(DocumentTitle);
