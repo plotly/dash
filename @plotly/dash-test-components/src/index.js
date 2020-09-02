b063d609b6e80145bac4e812934aa3bf5357eb49
harrisonn <griffin.harrisonn@gmail.com>
2020-09-02 13:20:10 -0400

    add test persistence components to @plotly/dash-test-components

--- @plotly/dash-test-components/src/index.js
+++ @plotly/dash-test-components/src/index.js
@@ -1,5 +1,8 @@
 import StyledComponent from './components/StyledComponent';
+import MyPersistedComponent from './components/MyPersistedComponent';
+import MyPersistedComponentNested from './components/MyPersistedComponentNested';
+
 
 export {
-    StyledComponent,
+    StyledComponent, MyPersistedComponent, MyPersistedComponentNested
 };
