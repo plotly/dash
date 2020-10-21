89540154b39837c51c11aad22f931e7634709352
Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>
2020-10-21 10:46:20 -0400

    Fix CI build swallowed errors (e.g. es5 check) (#872)

--- packages/dash-core-components/webpack.config.js
+++ packages/dash-core-components/webpack.config.js
@@ -65,7 +65,7 @@ module.exports = (env, argv) => {
                 },
                 {
                     test: /\.jsx?$/,
-                    include: /node_modules[\\\/](react-jsx-parser[\\\/])/,
+                    include: /node_modules[\\\/](react-jsx-parser|highlight[.]js)[\\\/]/,
                     use: {
                         loader: 'babel-loader',
                         options: {
