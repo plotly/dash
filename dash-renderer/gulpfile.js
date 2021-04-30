7f742ea0b00b675f150e99e5a1745bb053a1babb
hammadtheone <hammadtheone@gmail.com>
2021-04-30 17:10:55 -0400

    Updating gulpfile for version updates

--- dash-renderer/gulpfile.js
+++ dash-renderer/gulpfile.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra');
 const log = require('fancy-log')
 const print = require('gulp-print').default;
 const replace = require('gulp-replace');
+const packageJSON = require('./package.json')
 
 // Copy dash-renderer artifacts to dash directory
 function copyRendererArtifacts() {
@@ -16,19 +17,26 @@ function copyRendererArtifacts() {
         Please build the dash-renderer package before running this task.`)
 }
 
+
 // Update js_dist_dependencies in dash directory
 function updateRendererVersions() {
     if (fs.existsSync('dash_renderer/')) {
-        let rendererDeps = fs.readFileSync('dash_renderer/__init__.py').toString().split('\n')
-        rendererDeps = rendererDeps.slice(4).join('\n')
+        const dependencyVersions = {
+            'dash_renderer': packageJSON.version,
+            'polyfill': packageJSON.dependencies['@babel/polyfill'],
+            'react': packageJSON.dependencies.react,
+            'react_dom': packageJSON.dependencies['react-dom'],
+            'prop_types': packageJSON.dependencies['prop-types']
+        }
         return src('../dash/_dash_renderer.py')
-        .pipe(replace(/.*/s, rendererDeps))
+        .pipe(replace(/\{(.+?)\}/s, JSON.stringify(dependencyVersions, null, '    ')))
         .pipe(dest('../dash/', { overwrite: true }))
     }
 
     return log(`dash_renderer directory does not exist.
         Please build the dash-renderer package before running this task.`)
 }
+
 
 exports.merge_renderer = parallel(
     copyRendererArtifacts,
--- dash/_dash_renderer.py
+++ dash/_dash_renderer.py
@@ -1,31 +1,55 @@
+versions = {
+    "dash_renderer": "1.9.1",
+    "polyfill": "7.8.7",
+    "react": "16.14.0",
+    "react_dom": "16.14.0",
+    "prop_types": "15.7.2",
+}
+
 _js_dist_dependencies = [
     {
         "external_url": {
             "prod": [
-                "https://unpkg.com/@babel/polyfill@7.8.7/dist/polyfill.min.js",
-                "https://unpkg.com/react@16.14.0/umd/react.production.min.js",
-                "https://unpkg.com/react-dom@16.14.0/umd/react-dom.production.min.js",
-                "https://unpkg.com/prop-types@15.7.2/prop-types.min.js",
+                "https://unpkg.com/@babel/polyfill@{}/dist/polyfill.min.js".format(
+                    versions["polyfill"]
+                ),
+                "https://unpkg.com/react@{}/umd/react.production.min.js".format(
+                    versions["react"]
+                ),
+                "https://unpkg.com/react-dom@{}/umd/react-dom.production.min.js".format(
+                    versions["react_dom"]
+                ),
+                "https://unpkg.com/prop-types@{}/prop-types.min.js".format(
+                    versions["prop_types"]
+                ),
             ],
             "dev": [
-                "https://unpkg.com/@babel/polyfill@7.8.7/dist/polyfill.min.js",
-                "https://unpkg.com/react@16.14.0/umd/react.development.js",
-                "https://unpkg.com/react-dom@16.14.0/umd/react-dom.development.js",
-                "https://unpkg.com/prop-types@15.7.2/prop-types.js",
+                "https://unpkg.com/@babel/polyfill@{}/dist/polyfill.min.js".format(
+                    versions["polyfill"]
+                ),
+                "https://unpkg.com/react@{}/umd/react.development.js".format(
+                    versions["react"]
+                ),
+                "https://unpkg.com/react-dom@{}/umd/react-dom.development.js".format(
+                    versions["react_dom"]
+                ),
+                "https://unpkg.com/prop-types@{}/prop-types.js".format(
+                    versions["prop_types"]
+                ),
             ],
         },
         "relative_package_path": {
             "prod": [
-                "deps/polyfill@7.8.7.min.js",
-                "deps/react@16.14.0.min.js",
-                "deps/react-dom@16.14.0.min.js",
-                "deps/prop-types@15.7.2.min.js",
+                "deps/polyfill@{}.min.js".format(versions["polyfill"]),
+                "deps/react@{}.min.js".format(versions["react"]),
+                "deps/react-dom@{}.min.js".format(versions["react_dom"]),
+                "deps/prop-types@{}.min.js".format(versions["prop_types"]),
             ],
             "dev": [
-                "deps/polyfill@7.8.7.min.js",
-                "deps/react@16.14.0.js",
-                "deps/react-dom@16.14.0.js",
-                "deps/prop-types@15.7.2.js",
+                "deps/polyfill@{}.min.js".format(versions["polyfill"]),
+                "deps/react@{}.js".format(versions["react"]),
+                "deps/react-dom@{}.js".format(versions["react_dom"]),
+                "deps/prop-types@{}.js".format(versions["prop_types"]),
             ],
         },
         "namespace": "dash",
@@ -37,8 +61,8 @@ _js_dist = [
     {
         "relative_package_path": "deps/dash_renderer.min.js",
         "dev_package_path": "deps/dash_renderer.dev.js",
-        "external_url": "https://unpkg.com/dash-renderer@1.9.1"
-        "/dash_renderer/dash_renderer.min.js",
+        "external_url": "https://unpkg.com/dash-renderer@{}"
+        "/dash_renderer/dash_renderer.min.js".format(versions["dash_renderer"]),
         "namespace": "dash",
     },
     {
