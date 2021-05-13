dc740a10dfa74a5e16fb133445655281ece1d73a
Remi Baar <remibaar@gmail.com>
2021-05-13 17:12:54 +0200

    debug and dev_tools: Handle missing `timing_information` gracefully
    In some situations the `timing_information` is not present in `flask.g`. E.g. when other functions have been registerd to the flask `before_request_funcs`. Dash should anticipate and handle gracefully.
    
    Fixes #1624

--- dash/dash.py
+++ dash/dash.py
@@ -1468,10 +1468,16 @@ class Dash(object):
                 }
 
             def _after_request(response):
-                dash_total = flask.g.timing_information["__dash_server"]
-                dash_total["dur"] = round((time.time() - dash_total["dur"]) * 1000)
+                timing_information = flask.g.get("timing_information", None)
+                if timing_information is None:
+                    return response
 
-                for name, info in flask.g.timing_information.items():
+                timing_information_dash = timing_information.get("__dash_server", None)
+                if timing_information_dash is not None:
+                    dash_total = timing_information["__dash_server"]
+                    dash_total["dur"] = round((time.time() - dash_total["dur"]) * 1000)
+
+                for name, info in timing_information.items():
 
                     value = name
                     if info.get("desc") is not None:
