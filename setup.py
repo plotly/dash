dab606ab30e67629b36bc923096e90f80f80d03d
Alex Johnson <johnson.alex.c@gmail.com>
2021-05-18 11:29:12 -0400

    fix #1642 - add py 3.8 and 3.9 to classifiers

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Database :: Front-Ends",
         "Topic :: Office/Business :: Financial :: Spreadsheet",
         "Topic :: Scientific/Engineering :: Visualization",
