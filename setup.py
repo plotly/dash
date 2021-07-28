2984d48a8d7780892e95e9484e58dd669413f161
Jon Mease <jon.mease.dev@gmail.com>
2021-07-28 13:14:45 -0400

    Add back Python 3 classifier

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
