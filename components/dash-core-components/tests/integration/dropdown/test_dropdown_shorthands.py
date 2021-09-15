5f569c9e6a6eae2423a06cb54937a04baedf3fa6
workaholicpanda <>
2021-09-14 22:22:03 -0400

    chore: add labels to dropdown tests

--- components/dash-core-components/tests/integration/dropdown/test_dropdown_shorthands.py
+++ components/dash-core-components/tests/integration/dropdown/test_dropdown_shorthands.py
@@ -1,41 +1,50 @@
+from dash.dcc.Dropdown import Dropdown
 from dash import Dash, dcc, html
 
 
 def test_ddsh001_dropdown_shorthand_properties(dash_dcc):
     app = Dash(__name__)
-    app.layout = html.Div(
-        [
-            dcc.Dropdown(["a", "b", "c"]),
-            dcc.Dropdown(["a", "b", "c"], "b"),
-            dcc.Dropdown(["a", 3, "c"]),
-            dcc.Dropdown(["a", 3, "c"], 3),
-            dcc.Dropdown(["a", True, "c"]),
-            dcc.Dropdown(["a", True, "c"], True),
-            dcc.Dropdown(["a", 3, "c", True, False]),
-            dcc.Dropdown(["a", 3, "c", True, False], False),
-            dcc.Dropdown({"one": "One", "two": "Two", "three": "Three"}),
-            dcc.Dropdown({"one": "One", "two": "Two", "three": "Three"}, "two"),
-            dcc.Dropdown({"one": 1, "two": 2, "three": False}),
-            dcc.Dropdown({"one": 1, "two": 2, "three": False}, "three"),
-            dcc.Dropdown({"one": 1, "two": True, "three": 3}),
-            dcc.Dropdown({"one": 1, "two": True, "three": 3}, "two"),
-            dcc.Dropdown(
-                [
-                    {"label": "one", "value": 1},
-                    {"label": "two", "value": True},
-                    {"label": "three", "value": 3},
-                ]
-            ),
-            dcc.Dropdown(
-                [
-                    {"label": "one", "value": 1},
-                    {"label": "two", "value": True},
-                    {"label": "three", "value": 3},
-                ],
-                True,
-            ),
-        ]
-    )
+
+    TEST_OPTIONS_N_VALUES = [
+        (["a", "b", "c"]),
+        (["a", "b", "c"], "b"),
+        (["a", 3, "c"]),
+        (["a", 3, "c"], 3),
+        (["a", True, "c"]),
+        (["a", True, "c"], True),
+        (["a", 3, "c", True, False]),
+        (["a", 3, "c", True, False], False),
+        ({"one": "One", "two": "Two", "three": "Three"}),
+        ({"one": "One", "two": "Two", "three": "Three"}, "two"),
+        ({"one": 1, "two": 2, "three": False}),
+        ({"one": 1, "two": 2, "three": False}, "three"),
+        ({"one": 1, "two": True, "three": 3}),
+        ({"one": 1, "two": True, "three": 3}, "two"),
+        (
+            [
+                {"label": "one", "value": 1},
+                {"label": "two", "value": True},
+                {"label": "three", "value": 3},
+            ]
+        ),
+        (
+            [
+                {"label": "one", "value": 1},
+                {"label": "two", "value": True},
+                {"label": "three", "value": 3},
+            ],
+            True,
+        ),
+    ]
+
+    layout = []
+    for definition in TEST_OPTIONS_N_VALUES:
+        option, value = definition
+        layout.extend(
+            [html.Div(f"Options={option}, Value={value}"), dcc.Dropdown(option, value)]
+        )
+
+    app.layout = html.Div(layout)
 
     dash_dcc.start_server(app)
     dash_dcc.wait_for_element(".dash-dropdown")
