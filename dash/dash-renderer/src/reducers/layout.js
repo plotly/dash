63d7d8a0d0ddd29d8485c0b81bac4e79c4468988
Szabolcs Markó <akksi@akksi.eu>
2021-09-24 20:40:06 +0200

    Keep track of component property types or properties passed as {__value: 'value', __type: 'datatype'}

--- dash/dash-renderer/src/reducers/layout.js
+++ dash/dash-renderer/src/reducers/layout.js
@@ -1,10 +1,38 @@
-import {append, assocPath, includes, lensPath, mergeRight, view} from 'ramda';
+import {
+    append,
+    assocPath,
+    includes,
+    lensPath,
+    mergeRight,
+    view,
+    type
+} from 'ramda';
 
 import {getAction} from '../actions/constants';
 
+const processProperties = node => {
+    const updatedNode = {...node, propertyTypes: {}};
+    const {
+        props,
+        props: {children}
+    } = node;
+
+    if (type(children) === 'Array') {
+        updatedNode.props.children = children.map(child =>
+            processProperties(child)
+        );
+    }
+    Object.entries(props).forEach(([key, value]) => {
+        updatedNode.props[key] = value?.__value || value;
+        updatedNode.propertyTypes[key] = value?.__type || null;
+    });
+
+    return updatedNode;
+};
+
 const layout = (state = {}, action) => {
     if (action.type === getAction('SET_LAYOUT')) {
-        return action.payload;
+        return processProperties(action.payload);
     } else if (
         includes(action.type, [
             'UNDO_PROP_CHANGE',
@@ -13,9 +41,38 @@ const layout = (state = {}, action) => {
         ])
     ) {
         const propPath = append('props', action.payload.itempath);
+        const propertyTypesPath = append(
+            'propertyTypes',
+            action.payload.itempath
+        );
+
         const existingProps = view(lensPath(propPath), state);
         const mergedProps = mergeRight(existingProps, action.payload.props);
-        return assocPath(propPath, mergedProps, state);
+
+        const propertyValues = Object.entries(mergedProps).reduce(
+            (a, [key, value]) => {
+                return {...a, [key]: value.__value || value};
+            },
+            {}
+        );
+
+        const propertyTypes = Object.entries(mergedProps).reduce(
+            (a, [key, value]) => {
+                return {...a, [key]: value.__type || null};
+            },
+            {}
+        );
+
+        let updatedState = assocPath(propPath, propertyValues, state);
+        if (action.payload.source === 'response') {
+            updatedState = assocPath(
+                propertyTypesPath,
+                propertyTypes,
+                updatedState
+            );
+        }
+
+        return updatedState;
     }
 
     return state;
