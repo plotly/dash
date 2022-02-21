17af0b343a63f93f60366679da0140147b97338e
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2022-02-21 17:23:45 -0500

    mathjax in markdown

--- components/dash-core-components/src/components/Markdown.react.js
+++ components/dash-core-components/src/components/Markdown.react.js
@@ -1,8 +1,8 @@
+import {asyncDecorator} from '@plotly/dash-component-plugins';
 import PropTypes from 'prop-types';
-import React, {Component, lazy, Suspense} from 'react';
+import React, {Component, Suspense} from 'react';
 import markdown from '../utils/LazyLoader/markdown';
-
-const RealDashMarkdown = lazy(markdown);
+import mathjax from '../utils/LazyLoader/mathjax';
 
 // eslint-disable-next-line valid-jsdoc
 /**
@@ -11,6 +11,14 @@ const RealDashMarkdown = lazy(markdown);
  * [react-markdown](https://rexxars.github.io/react-markdown/) under the hood.
  */
 export default class DashMarkdown extends Component {
+    constructor(props) {
+        super(props);
+
+        if (props.mathjax) {
+            DashMarkdown._loadMathjax = true;
+        }
+    }
+
     render() {
         return (
             <Suspense fallback={null}>
@@ -31,6 +39,11 @@ DashMarkdown.propTypes = {
      * Class name of the container element
      */
     className: PropTypes.string,
+
+    /**
+     * If true, loads mathjax v3 (tex-svg) into the page and use it in the markdown
+     */
+    mathjax: PropTypes.bool,
 
     /**
      * A boolean to control raw HTML escaping.
@@ -91,10 +104,18 @@ DashMarkdown.propTypes = {
 };
 
 DashMarkdown.defaultProps = {
+    mathjax: false,
     dangerously_allow_html: false,
     highlight_config: {},
     dedent: true,
 };
+
+const RealDashMarkdown = asyncDecorator(DashMarkdown, () =>
+    Promise.all([
+        markdown(),
+        DashMarkdown._loadMathjax ? mathjax() : undefined,
+    ]).then(([md]) => md)
+);
 
 export const propTypes = DashMarkdown.propTypes;
 export const defaultProps = DashMarkdown.defaultProps;
---
+++ components/dash-core-components/src/fragments/Math.react.js
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+
+export default class DashMath extends Component {
+    constructor(props) {
+        super(props);
+        this.span_element = React.createRef();
+    }
+
+    componentDidMount() {
+        this.renderMath();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (
+            prevProps.tex !== this.props.tex ||
+            prevProps.inline !== this.props.inline
+        ) {
+            this.renderMath();
+        }
+    }
+
+    renderMath() {
+        if (window.MathJax && window.MathJax.typeset) {
+            window.MathJax.typeset([this.span_element.current]);
+        }
+    }
+
+    render() {
+        return (
+            <span ref={this.span_element}>
+                {this.props.inline ? String.raw`\(` : String.raw`\[`}
+                {this.props.tex}
+                {this.props.inline ? String.raw`\)` : String.raw`\]`}
+            </span>
+        );
+    }
+}
+
+DashMath.propTypes = {
+    tex: PropTypes.string,
+    inline: PropTypes.bool,
+};
+
+DashMath.defaultProps = {
+    tex: '',
+    inline: true,
+};
+
+export const propTypes = DashMath.propTypes;
+export const defaultProps = DashMath.defaultProps;
