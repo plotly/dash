a01a47123ef5d41ae3b0e1fd2158bb4193bfed67
Andrii Oriekhov <andriyorehov@gmail.com>
2022-03-02 11:46:13 +0200

    add GitHub URL for PyPi

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ setup(
         "pytest11": ["dash = dash.testing.plugin"],
     },
     url="https://plotly.com/dash",
+    project_urls={
+        "Source": "https://github.com/plotly/dash",
+    },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
