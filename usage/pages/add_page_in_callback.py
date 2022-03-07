25999219f56957c37cdc2c45254e52862775331a
AnnMarieW <amward@fastmail.com>
2022-03-07 07:07:32 -0700

    new usage page - need to add a test

---
+++ usage/pages/add_page_in_callback.py
@@ -0,0 +1,25 @@
+from dash import html, dcc, callback, Input, Output
+
+import dash
+
+dash.register_page(__name__)
+
+layout = (html.Button("Add a Page Button", id="button"), html.Div(id="content"))
+
+
+@callback(
+    Output("content", "children"),
+    Input("button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def update_output_div(n_clicks):
+
+    dash.register_page(
+        "callback_page",
+        path=f"/callback-page/{n_clicks}",
+        redirect_from=["/callback-page"],
+        layout=html.Div(["Callback Page"]),
+    )
+    #  import pprint
+    #  pprint.pprint(dash.page_registry)
+    return f"New page created:  redirect_from= /callback-page   path= /callback-page/{n_clicks}"
