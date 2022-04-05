3912c81df562e2055b85abe38688944df96711d2
philippe <t4rk@outlook.com>
2022-04-05 19:42:39 -0400

    Add more Chrome browser arguments.

--- dash/testing/browser.py
+++ dash/testing/browser.py
@@ -465,6 +465,10 @@ class Browser(DashPageMixin):
                 "safebrowsing.disable_download_protection": True,
             },
         )
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--remote-debugging-port=9222")
 
         chrome = (
             webdriver.Remote(
