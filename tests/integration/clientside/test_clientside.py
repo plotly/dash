54e2e3d8135c96e5aed867bea9c3e058957ff280
Lachlan Teale <MrTeale@users.noreply.github.com>
2022-04-22 03:17:39 +1000

    Typo

--- tests/integration/clientside/test_clientside.py
+++ tests/integration/clientside/test_clientside.py
@@ -828,4 +828,4 @@ def test_clsd019_clientside_inline_promise(dash_duo):
     )
 
     dash_duo.start_server(app)
-    dash_duo.wait_for_text_to_equal("#output-div", "initial-nline")
+    dash_duo.wait_for_text_to_equal("#output-div", "initial-inline")
