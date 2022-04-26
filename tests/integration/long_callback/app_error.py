dbacfcc6da64052d31697f57922a4e9e1066df4c
philippe <t4rk@outlook.com>
2022-04-26 16:25:20 -0400

    :hankey: Add test long callback error.

---
+++ tests/integration/long_callback/app_error.py
@@ -0,0 +1,42 @@
+import time
+
+import dash
+from dash import html
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+from tests.integration.long_callback.utils import get_long_callback_manager
+
+long_callback_manager = get_long_callback_manager()
+handle = long_callback_manager.handle
+
+app = dash.Dash(__name__, long_callback_manager=long_callback_manager)
+app.enable_dev_tools(debug=True, dev_tools_ui=True)
+app.layout = html.Div(
+    [
+        html.Div([html.P(id="output", children=["Button not clicked"])]),
+        html.Button(id="button", children="Run Job!"),
+    ]
+)
+
+
+@app.long_callback(
+    output=Output("output", "children"),
+    inputs=Input("button", "n_clicks"),
+    running=[
+        (Output("button", "disabled"), True, False),
+    ],
+    prevent_initial_call=True,
+)
+def callback(n_clicks):
+    time.sleep(0.5)
+    if n_clicks == 2:
+        raise Exception("bad error")
+
+    if n_clicks == 4:
+        raise PreventUpdate
+    return f"Clicked {n_clicks} times"
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- tests/integration/long_callback/test_basic_long_callback.py
+++ tests/integration/long_callback/test_basic_long_callback.py
@@ -426,3 +426,26 @@ def test_lcbc007_validation_layout(dash_duo, manager):
 
     assert not dash_duo.redux_state_is_loading
     assert dash_duo.get_logs() == []
+
+
+def test_lcbc008_long_callbacks_error(dash_duo, manager):
+    with setup_long_callback_app(manager, "app_error") as app:
+        dash_duo.start_server(app)
+
+        clicker = dash_duo.find_element("#button")
+
+        clicker.click()
+        dash_duo.wait_for_text_to_equal("#output", "Clicked 1 times")
+
+        clicker.click()
+        dash_duo.wait_for_element("#button:not([disabled])")
+        # dash_duo.wait_for_contains_text('.dash-fe-error__title' 'An error occurred inside a long callback:')
+
+        clicker.click()
+        dash_duo.wait_for_text_to_equal("#output", "Clicked 3 times")
+
+        clicker.click()
+        clicker = dash_duo.wait_for_element("#button:not([disabled])")
+        dash_duo.wait_for_text_to_equal("#output", "Clicked 3 times")
+        clicker.click()
+        dash_duo.wait_for_text_to_equal("#output", "Clicked 5 times")
