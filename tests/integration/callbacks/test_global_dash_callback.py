f1a479655a32ed88ac7ea9b7d949a6a7e75b9620
alexcjohnson <alex@plot.ly>
2022-05-24 12:53:58 -0400

    run dash.callback test twice to test dependence on global state

--- tests/integration/callbacks/test_global_dash_callback.py
+++ tests/integration/callbacks/test_global_dash_callback.py
@@ -1,8 +1,12 @@
+import pytest
+
 import dash
 from dash import Input, Output, dcc, html
 
 
-def test_dash_callback_001(dash_duo):
+@pytest.mark.parametrize('rep', [1, 2])
+def test_dash_callback_001(dash_duo, rep):
+    # run this test twice to ensure global state is properly reset between runs
     app = dash.Dash(__name__)
 
     app.layout = html.Div(
@@ -18,27 +22,27 @@ def test_dash_callback_001(dash_duo):
 
     @dash.callback(Output("div-1", "children"), Input("input", "value"))
     def update_1(value):  # pylint: disable=unused-variable
-        return f"Input 1 - {value}"
+        return f"Input 1 - rep {rep} - {value}"
 
     @dash.callback(Output("div-2", "children"), Input("input", "value"))
     def update_2(value):  # pylint: disable=unused-variable
-        return f"Input 2 - {value}"
+        return f"Input 2 - rep {rep} - {value}"
 
     @app.callback(Output("div-3", "children"), Input("input", "value"))
     def update_3(value):  # pylint: disable=unused-variable
-        return f"Input 3 - {value}"
+        return f"Input 3 - rep {rep} - {value}"
 
     app.clientside_callback(
-        """
-        function (args) {return ('Input 4 - ' + args);}
+        f"""
+        function (args) {{return ('Input 4 - rep {rep} - ' + args);}}
         """,
         Output("div-4", "children"),
         Input("input", "value"),
     )
 
     dash.clientside_callback(
-        """
-        function (args) {return ('Input 5 - ' + args);}
+        f"""
+        function (args) {{return ('Input 5 - rep {rep} - ' + args);}}
         """,
         Output("div-5", "children"),
         Input("input", "value"),
@@ -47,8 +51,8 @@ def test_dash_callback_001(dash_duo):
     dash_duo.start_server(app)
     input_element = dash_duo.find_element("#input")
     input_element.send_keys("dash.callback")
-    dash_duo.wait_for_text_to_equal("#div-1", "Input 1 - dash.callback")
-    dash_duo.wait_for_text_to_equal("#div-2", "Input 2 - dash.callback")
-    dash_duo.wait_for_text_to_equal("#div-3", "Input 3 - dash.callback")
-    dash_duo.wait_for_text_to_equal("#div-4", "Input 4 - dash.callback")
-    dash_duo.wait_for_text_to_equal("#div-5", "Input 5 - dash.callback")
+    dash_duo.wait_for_text_to_equal("#div-1", f"Input 1 - rep {rep} - dash.callback")
+    dash_duo.wait_for_text_to_equal("#div-2", f"Input 2 - rep {rep} - dash.callback")
+    dash_duo.wait_for_text_to_equal("#div-3", f"Input 3 - rep {rep} - dash.callback")
+    dash_duo.wait_for_text_to_equal("#div-4", f"Input 4 - rep {rep} - dash.callback")
+    dash_duo.wait_for_text_to_equal("#div-5", f"Input 5 - rep {rep} - dash.callback")
