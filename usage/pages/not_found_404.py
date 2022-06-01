b4905239c56615d4e6d8cad8c2028e116adb1a4b
AnnMarieW <amward@fastmail.com>
2022-06-01 11:16:50 -0700

    delete usage folder

--- usage/pages/not_found_404.py
+++
@@ -1,9 +0,0 @@
-#  Example of custom not found page
-
-from dash import html
-import dash
-
-dash.register_page(__name__, path="/404")
-
-
-layout = html.H1("Custom 404")
--- usage/pages/query_string.py
+++
@@ -1,8 +0,0 @@
-# example of query strings. Try entering ?velocity=20 at the end of the url.
-import dash
-
-dash.register_page(__name__, path="/dashboard")
-
-
-def layout(velocity=None, **other_unknown_query_strings):
-    return dash.html.Div([dash.dcc.Input(id="velocity", value=velocity)])
