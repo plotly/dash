55455f952d3c68fc7d30de8378088efa4da9a7f7
philippe <t4rk@outlook.com>
2022-06-15 11:12:53 -0400

    Add test short interval.

--- tests/integration/long_callback/test_basic_long_callback.py
+++ tests/integration/long_callback/test_basic_long_callback.py
@@ -481,3 +481,16 @@ def test_lcbc008_long_callbacks_error(dash_duo, manager):
             dash_duo.wait_for_text_to_equal("#output-status", f"Updated: {i}")
             for j, e in enumerate(expect):
                 assert dash_duo.find_element(f"#output{j + 1}").text == e
+
+
+def test_lcbc009_short_interval(dash_duo, manager):
+    with setup_long_callback_app(manager, "app_short_interval") as app:
+        dash_duo.start_server(app)
+        dash_duo.find_element("#run-button").click()
+        dash_duo.wait_for_text_to_equal("#status", "Progress 2/4", 20)
+        dash_duo.wait_for_text_to_equal("#status", "Finished", 12)
+        dash_duo.wait_for_text_to_equal("#result", "Clicked '1'")
+
+        time.sleep(2)
+        # Ensure the progress is still not running
+        assert dash_duo.find_element("#status").text == "Finished"
