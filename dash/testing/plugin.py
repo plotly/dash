2cd87ccbcd1ef68c8d6c48ff418eb2291290282c
philippe <t4rk@outlook.com>
2022-08-09 12:02:35 -0400

    Improve error message when using fixtures but testing not installed.

--- dash/testing/plugin.py
+++ dash/testing/plugin.py
@@ -1,8 +1,15 @@
 # pylint: disable=missing-docstring,redefined-outer-name
-from typing import Any
-
 import pytest
 from .consts import SELENIUM_GRID_DEFAULT
+
+
+# pylint: disable=too-few-public-methods
+class MissingDashTesting:
+    def __init__(self, **kwargs):
+        raise Exception(
+            "dash[testing] was not installed. "
+            "Please install to use the dash testing fixtures."
+        )
 
 
 try:
@@ -16,14 +23,14 @@ try:
     from dash.testing.composite import DashComposite, DashRComposite, DashJuliaComposite
 except ImportError:
     # Running pytest without dash[testing] installed.
-    ThreadedRunner = Any
-    ProcessRunner = Any
-    RRunner = Any
-    JuliaRunner = Any
-    Browser = Any
-    DashComposite = Any
-    DashRComposite = Any
-    DashJuliaComposite = Any
+    ThreadedRunner = MissingDashTesting
+    ProcessRunner = MissingDashTesting
+    RRunner = MissingDashTesting
+    JuliaRunner = MissingDashTesting
+    Browser = MissingDashTesting
+    DashComposite = MissingDashTesting
+    DashRComposite = MissingDashTesting
+    DashJuliaComposite = MissingDashTesting
 
 
 def pytest_addoption(parser):
