e139906c66f5be1bc2b3535e668963a8fa320f0e
siner308 <siner308@gmail.com>
2022-09-08 02:57:23 +0000

    add parentheses

--- @plotly/dash-generator-test-component-typescript/src/components/RequiredChildrenComponent.tsx
+++ @plotly/dash-generator-test-component-typescript/src/components/RequiredChildrenComponent.tsx
@@ -4,9 +4,11 @@ import { RequiredChildrenComponentProps } from "../props";
 
 const RequiredChildrenComponent = (props: RequiredChildrenComponentProps) => {
   const {children} = props;
-  return <div>
-    {children}
-  </div>
+  return (
+    <div>
+      {children}
+    </div>
+  )
 }
 
 export default RequiredChildrenComponent;
