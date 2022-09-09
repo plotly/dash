ef84089232477940c25154465ebbe691e42c7dcb
AnnMarieW <amward@fastmail.com>
2022-09-09 16:58:50 -0700

    improved tests

--- tests/integration/multi_page/pages/.no_import/no_import.py
+++ tests/integration/multi_page/pages/.no_import/no_import.py
@@ -1,1 +1,3 @@
+from dash import register_page
+
 raise Exception("files in directories starting with . should not be imported")
--- tests/integration/multi_page/pages/_no_import.py
+++ tests/integration/multi_page/pages/_no_import.py
@@ -1,1 +1,3 @@
+from dash import register_page
+
 raise Exception("files starting with _ should not be imported")
