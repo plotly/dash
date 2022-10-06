0168242641b569e7a3ecc2bc76078e9d03316d1f
alexcjohnson <alex@plot.ly>
2022-10-06 13:09:02 -0400

    render -> createRoot

--- dash/dash-renderer/src/DashRenderer.js
+++ dash/dash-renderer/src/DashRenderer.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {createRoot} from 'react-dom/client';
 import AppProvider from './AppProvider.react';
 
 class DashRenderer {
     constructor(hooks) {
         // render Dash Renderer upon initialising!
-        ReactDOM.render(
-            <AppProvider hooks={hooks} />,
-            document.getElementById('react-entry-point')
-        );
+        const container = document.getElementById('react-entry-point');
+
+        if (ReactDOM.createRoot) {
+            const root = ReactDOM.createRoot(container);
+            root.render(<AppProvider hooks={hooks} />);
+        } else {
+            ReactDOM.render(<AppProvider hooks={hooks} />, container);
+        }
     }
 }
 
