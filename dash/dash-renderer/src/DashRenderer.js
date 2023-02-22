f4efee95c619ecd5e6c4e8ff98ce7f4c15acb9e9
philippe <t4rk@outlook.com>
2023-02-22 11:32:34 -0500

    Remove commented import.

--- dash/dash-renderer/src/DashRenderer.js
+++ dash/dash-renderer/src/DashRenderer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {createRoot} from 'react-dom/client';
+
 import AppProvider from './AppProvider.react';
 
 class DashRenderer {
