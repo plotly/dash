32d25b57ac3f58d59f16bb63ad3b78feb2c5842b
philippe <t4rk@outlook.com>
2023-03-28 16:13:04 -0400

    Add test background callback diff outputs same func.

--- dash/long_callback/managers/__init__.py
+++ dash/long_callback/managers/__init__.py
@@ -99,7 +99,9 @@ class BaseLongCallbackManager(ABC):
         return key + "-progress"
 
     @staticmethod
-    def hash_function(fn, callback_id=''):
+    def hash_function(fn, callback_id=""):
         fn_source = inspect.getsource(fn)
         fn_str = fn_source
-        return hashlib.sha1(callback_id.encode("utf-8") + fn_str.encode("utf-8")).hexdigest()
+        return hashlib.sha1(
+            callback_id.encode("utf-8") + fn_str.encode("utf-8")
+        ).hexdigest()
