e5f2ddbdaf92d9f9f5193861ad6a8a621f3b573f
Raphael Nestler <raphael.nestler@renuo.ch>
2023-06-02 09:53:53 +0200

    Check if module is present before removing it
    Apparently not all tests that add modules to the PAGE_REGISTRY also add
    them to sys.modules.

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,8 @@ def clear_pages_state():
 def init_pages_state():
     """Clear all global state that is used by pages feature."""
     for page in dash._pages.PAGE_REGISTRY.values():
-        sys.modules.pop(page["module"])
+        if page["module"] in sys.modules:
+            sys.modules.pop(page["module"])
     dash._pages.PAGE_REGISTRY.clear()
     dash._pages.CONFIG.clear()
     dash._pages.CONFIG.__dict__.clear()
