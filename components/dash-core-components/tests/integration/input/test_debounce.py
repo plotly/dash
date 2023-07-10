259b63a4c7eae929feddd16667143e58961e35f4
Adrian Borrmann <hello@adrianborrmann.com>
2023-07-10 14:47:14 -0600

    Update selenium import for older python compatibility

--- components/dash-core-components/tests/integration/input/test_debounce.py
+++ components/dash-core-components/tests/integration/input/test_debounce.py
@@ -1,4 +1,4 @@
-from selenium.common import TimeoutException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 import pytest
@@ -48,9 +48,9 @@ def test_debounce_number_by_time(dash_dcc, debounce_number_app):
 
     # expect that a short debounce calls back within a short amount of time
     elem = dash_dcc.find_element("#input-fast")
-    elem.send_keys("67890")
+    elem.send_keys("10000")
     WebDriverWait(dash_dcc.driver, timeout=1).until(
-        lambda d: d.find_element(By.XPATH, "//*[text()='67890']")
+        lambda d: d.find_element(By.XPATH, "//*[text()='10000']")
     )
 
     assert dash_dcc.get_logs() == []
