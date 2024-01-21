b3a038635b7139190b34b8fd82580bce12e56ae7
zeshan <zeshan.14jul1789@gmail.com>
2024-01-20 19:37:11 -0600

    :dolls: Removed the nested feature list which was causing the formatting issues. Replaced it with a heading for the section and paragraph separated portions for each function parameter.

--- dash/_callback.py
+++ dash/_callback.py
@@ -81,12 +81,15 @@ def callback(
     not to fire when its outputs are first added to the page. Defaults to
     `False` and unlike `app.callback` is not configurable at the app level.
 
-    :Keyword Arguments:
-        :param background:
+    Keyword Arguments
+    -----------------
+    
+    param background:
             Mark the callback as a long callback to execute in a manager for
             callbacks that take a long time without locking up the Dash app
             or timing out.
-        :param manager:
+            
+    param manager:
             A long callback manager instance. Currently, an instance of one of
             `DiskcacheManager` or `CeleryManager`.
             Defaults to the `background_callback_manager` instance provided to the
@@ -98,19 +101,22 @@ def callback(
             - A Celery manager (`CeleryManager`) that runs callback logic
               in a celery worker and returns results to the Dash app through a Celery
               broker like RabbitMQ or Redis.
-        :param running:
+              
+    param running:
             A list of 3-element tuples. The first element of each tuple should be
             an `Output` dependency object referencing a property of a component in
             the app layout. The second element is the value that the property
             should be set to while the callback is running, and the third element
             is the value the property should be set to when the callback completes.
-        :param cancel:
+            
+    param cancel:
             A list of `Input` dependency objects that reference a property of a
             component in the app's layout.  When the value of this property changes
             while a callback is running, the callback is canceled.
             Note that the value of the property is not significant, any change in
             value will result in the cancellation of the running job (if any).
-        :param progress:
+            
+    param progress:
             An `Output` dependency grouping that references properties of
             components in the app's layout. When provided, the decorated function
             will be called with an extra argument as the first argument to the
@@ -119,18 +125,21 @@ def callback(
             current progress. This function accepts a single argument, which
             correspond to the grouping of properties specified in the provided
             `Output` dependency grouping
-        :param progress_default:
+            
+    param progress_default:
             A grouping of values that should be assigned to the components
             specified by the `progress` argument when the callback is not in
             progress. If `progress_default` is not provided, all the dependency
             properties specified in `progress` will be set to `None` when the
             callback is not running.
-        :param cache_args_to_ignore:
+            
+    param cache_args_to_ignore:
             Arguments to ignore when caching is enabled. If callback is configured
             with keyword arguments (Input/State provided in a dict),
             this should be a list of argument names as strings. Otherwise,
             this should be a list of argument indices as integers.
-        :param interval:
+            
+    param interval:
             Time to wait between the long callback update requests.
     """
 
