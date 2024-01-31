bc0e46e04fa5f57fb2aae69e2709d4b5d37b0074
Thomas Grainger <thomas.grainger@coefficientai.onmicrosoft.com>
2024-01-31 13:34:09 +0000

    vendor preconditions
    preconditions has been unmaintained for 9 years and calls the
    removed function inspect.getargspec

--- components/dash-table/tests/selenium/conftest.py
+++ components/dash-table/tests/selenium/conftest.py
@@ -1,13 +1,96 @@
 import platform
 import pytest
+from functools import wraps
+import inspect
 
 from dash.testing.browser import Browser
-from preconditions import preconditions
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+
+
+class PreconditionError(TypeError):
+    pass
+
+
+def preconditions(*precs):
+    stripped_source = lambda obj: inspect.getsource(obj).strip()
+
+    if not precs:
+        # This edge case makes ``@preconditions()`` efficiently delegate
+        # to the wrapped function, which I anticipate will be useful
+        # for stubbing and code consistency in applications:
+        def null_decorator(f):
+            f.nopre = f  # Meet the .nopre interface requirement.
+            return f
+
+        return null_decorator
+
+    precinfo = []
+    for p in precs:
+        spec = inspect.getargspec(p)
+        if spec.varargs or spec.keywords:
+            raise PreconditionError(
+                (
+                    "Invalid precondition must not accept * nor ** args:\n" + "  {!s}\n"
+                ).format(stripped_source(p))
+            )
+
+        i = -len(spec.defaults or ())
+        if i == 0:
+            appargs, closureargs = spec.args, []
+        else:
+            appargs, closureargs = spec.args[:i], spec.args[i:]
+        precinfo.append((appargs, closureargs, p))
+
+    def decorate(f):
+        fspec = inspect.getargspec(f)
+
+        for (appargs, closureargs, p) in precinfo:
+            for apparg in appargs:
+                if apparg not in fspec.args:
+                    raise PreconditionError(
+                        (
+                            "Invalid precondition refers to unknown parameter {!r}:\n"
+                            + "  {!s}\n"
+                            + "Known parameters: {!r}\n"
+                        ).format(apparg, stripped_source(p), fspec.args)
+                    )
+            for carg in closureargs:
+                if carg in fspec.args:
+                    raise PreconditionError(
+                        (
+                            "Invalid precondition masks parameter {!r}:\n"
+                            + "  {!s}\n"
+                            + "Known parameters: {!r}\n"
+                        ).format(carg, stripped_source(p), fspec.args)
+                    )
+
+        @wraps(f)
+        def g(*a, **kw):
+            args = inspect.getcallargs(f, *a, **kw)
+            for (appargs, _, p) in precinfo:
+                if not p(*[args[aa] for aa in appargs]):
+                    raise PreconditionError(
+                        "Precondition failed in call {!r}{}:\n  {!s}\n".format(
+                            g,
+                            inspect.formatargvalues(
+                                fspec.args, fspec.varargs, fspec.keywords, args
+                            ),
+                            stripped_source(p),
+                        )
+                    )
+
+            return f(*a, **kw)
+
+        g.nopre = f
+        return g
+
+    return decorate
+
 
 _validate_col = lambda col: (isinstance(col, str) and len(col) > 0) or (
     isinstance(col, int) and col >= 0
