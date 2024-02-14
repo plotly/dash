9f5ac4064f3e8e83a372239fe6846932af05f2fa
BSd3v <82055130+BSd3v@users.noreply.github.com>
2024-02-14 17:02:39 -0500

    adding ignore for  `window` object again

--- dash/dash-renderer/src/utils/setProps.ts
+++ dash/dash-renderer/src/utils/setProps.ts
@@ -21,6 +21,7 @@ const setProps = (updates: {}) => {
     }
 };
 
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 const dc = (window.dash_clientside = window.dash_clientside || {});
 dc['setProps'] = setProps;
