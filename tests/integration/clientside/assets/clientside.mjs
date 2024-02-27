93877fdd443f7da6caaac78828fa53e64f3054c6
Jonas <code@jowlo.de>
2024-02-27 09:46:37 +0100

    Use existing window.dash_clientside if already set
    Co-authored-by: Alex Johnson <alex@plot.ly>

--- tests/integration/clientside/assets/clientside.mjs
+++ tests/integration/clientside/assets/clientside.mjs
@@ -1,5 +1,6 @@
 import {display} from "./clientsideModule.mjs";
 
+window.dash_clientside = window.dash_clientside || {};
 window.dash_clientside.clientside_module = Object.assign({}, window.dash_clientside.clientside_module, {
 	display
 });
