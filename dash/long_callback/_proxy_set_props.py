dfdb6056496443462435d9aa0590c42135cd0d69
philippe <t4rk@outlook.com>
2024-04-23 14:40:26 -0400

    Add ProxySetProps docstring

--- dash/long_callback/_proxy_set_props.py
+++ dash/long_callback/_proxy_set_props.py
@@ -1,4 +1,9 @@
 class ProxySetProps(dict):
+    """
+    Defer dictionary item setter to run a custom function on change.
+    Used by background callback manager to save the `set_props` data.
+    """
+
     def __init__(self, on_change):
         super().__init__()
         self.on_change = on_change
