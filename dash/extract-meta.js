d6e2ecf2200dc1a17a5b38b3076e82b82744d669
Samuel Hinshaw <samuel.hinshaw@plot.ly>
2024-07-29 21:36:35 -0700

    support typescript 5.5

--- dash/extract-meta.js
+++ dash/extract-meta.js
@@ -37,24 +37,7 @@ if (!src.length) {
 }
 
 if (fs.existsSync('tsconfig.json')) {
-    tsconfig = JSON.parse(fs.readFileSync('tsconfig.json')).compilerOptions;
-    // Map moduleResolution to the appropriate enum.
-    switch (tsconfig.moduleResolution) {
-        case 'node':
-            tsconfig.moduleResolution = ts.ModuleResolutionKind.NodeJs;
-            break;
-        case 'node16':
-            tsconfig.moduleResolution = ts.ModuleResolutionKind.Node16;
-            break;
-        case 'nodenext':
-            tsconfig.moduleResolution = ts.ModuleResolutionKind.NodeNext;
-            break;
-        case 'classic':
-            tsconfig.moduleResolution = ts.ModuleResolutionKind.Classic;
-            break;
-        default:
-            break;
-    }
+    tsconfig = ts.getParsedCommandLineOfConfigFile('tsconfig.json', {}, ts.sys);
 }
 
 let failedBuild = false;
