d99da2a374e6e151ef136edb870bc78a0dd0c08e
philippe <t4rk@outlook.com>
2024-08-28 09:28:28 -0400

    Add clear_pages_state to arb003

--- tests/integration/callbacks/test_arbitrary_callbacks.py
+++ tests/integration/callbacks/test_arbitrary_callbacks.py
@@ -11,8 +11,6 @@ from dash import (
     clientside_callback,
 )
 
-from flaky import flaky
-
 
 def test_arb001_global_set_props(dash_duo):
     app = Dash()
@@ -91,8 +89,7 @@ def test_arb002_no_output_callbacks(dash_duo):
         assert counter.value == 1
 
 
-@flaky(max_runs=3)
-def test_arb003_arbitrary_pages(dash_duo):
+def test_arb003_arbitrary_pages(dash_duo, clear_pages_state):
     app = Dash(use_pages=True, pages_folder="")
 
     register_page(
