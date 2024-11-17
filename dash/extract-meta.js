6b0c458f85332806fc2fcfaa648af0b3876632b4
Alex Johnson <johnson.alex.c@gmail.com>
2024-11-17 18:08:49 -0500

    More flexible reading of component docstrings
    To support single-line comments or nonstandard indents

--- dash/extract-meta.js
+++ dash/extract-meta.js
@@ -762,7 +762,7 @@ function gatherComponents(sources, components = {}) {
                         fullText
                             .slice(r.pos + 4, r.end - 3)
                             .split('\n')
-                            .map(s => s.slice(3, s.length))
+                            .map(s => s.replace(/^(\s*\*?\s+)/, ''))
                             .filter(e => e)
                             .join('\n')
                     )
