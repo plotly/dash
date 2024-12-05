ada24e53ed62be74474955e68866772170451e9e
philippe <t4rk@outlook.com>
2024-12-05 10:18:26 -0500

    sleep longer

--- components/dash-core-components/tests/integration/sliders/test_sliders_shorthands.py
+++ components/dash-core-components/tests/integration/sliders/test_sliders_shorthands.py
@@ -193,5 +193,5 @@ def test_slsh002_sliders_marks_si_unit_format(dash_dcc):
 
     dash_dcc.start_server(app)
     dash_dcc.wait_for_element(".rc-slider")
-    time.sleep(1)
+    time.sleep(2)
     dash_dcc.percy_snapshot("slsh002 - test_slsh002_sliders_marks_si_unit_format", True)
