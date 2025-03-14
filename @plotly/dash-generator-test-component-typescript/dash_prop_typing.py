ec75aca5650876dc7280654d00b402d7f99925ac
philippe <t4rk@outlook.com>
2025-03-14 15:13:05 -0400

    fix test_typing

--- @plotly/dash-generator-test-component-typescript/dash_prop_typing.py
+++ @plotly/dash-generator-test-component-typescript/dash_prop_typing.py
@@ -1,14 +1,1 @@
 ignore_props = ['ignored_prop']
-
-custom_imports = {
-    "*": ["import json"]
-}
-
-def generate_style(*_):
-    return "dict"
-
-custom_props = {
-    "*": {
-        "obj": generate_style
-    }
-}
