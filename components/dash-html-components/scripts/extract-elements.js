2fd72c6d9d52ebcb973b4645ae3ada24f6d04e6f
philippe <t4rk@outlook.com>
2025-05-06 09:26:13 -0400

    Exclude selectedcontent html element

--- components/dash-html-components/scripts/extract-elements.js
+++ components/dash-html-components/scripts/extract-elements.js
@@ -22,7 +22,8 @@ function extractElements($) {
         'noframes',
         // experimental, don't add yet
         'portal',
-        'fencedframe'
+        'fencedframe',
+        'selectedcontent',  // No support beside latest chrome
     ];
     // `<section>` is for some reason missing from the reference tables.
     const addElements = [
