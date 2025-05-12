7cabf51f5dea44dbac78701a8fc7069e35ae656e
philippe <t4rk@outlook.com>
2025-05-12 10:47:23 -0400

    Do not change dir in typing test

--- tests/compliance/test_typing.py
+++ tests/compliance/test_typing.py
@@ -349,10 +349,9 @@ if sys.version_info.minor >= 10:
     ],
 )
 def test_typi002_typing_compliance(
-    typing_module, prelayout, layout, callback_return, assertions, tmp_path, change_dir
+    typing_module, prelayout, layout, callback_return, assertions, tmp_path
 ):
     codefile = os.path.join(tmp_path, "code.py")
-    os.chdir(tmp_path)
     code = format_template_and_save(
         basic_app_template, codefile, prelayout, layout, callback_return
     )
