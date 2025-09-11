ed0dc3b4fbdf78ea68ca1d21e510aca1bf4a3320
Christian Giessel <cgiessel@tesla.com>
2025-09-11 21:24:55 +0200

    ∙ - initial quart factory ∙ - added types to BaseFactory to remove linting errors on create app in Flask and Quart Factory

--- dash/server_factories/base_factory.py
+++ dash/server_factories/base_factory.py
@@ -1,49 +1,50 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class BaseServerFactory(ABC):
-    def __call__(self, server, *args, **kwargs):
+    def __call__(self, server, *args, **kwargs) -> Any:
         # Default: WSGI
         return server(*args, **kwargs)
 
     @abstractmethod
-    def create_app(self, name="__main__", config=None):
+    def create_app(self, name: str = "__main__", config=None) -> Any:  # pragma: no cover - interface
         pass
 
     @abstractmethod
     def register_assets_blueprint(
-        self, app, blueprint_name, assets_url_path, assets_folder
-    ):
+        self, app, blueprint_name: str, assets_url_path: str, assets_folder: str
+    ) -> None:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def register_error_handlers(self, app):
+    def register_error_handlers(self, app) -> None:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def add_url_rule(self, app, rule, view_func, endpoint=None, methods=None):
+    def add_url_rule(self, app, rule: str, view_func, endpoint=None, methods=None) -> None:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def before_request(self, app, func):
+    def before_request(self, app, func) -> None:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def after_request(self, app, func):
+    def after_request(self, app, func) -> None:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def run(self, app, host, port, debug, **kwargs):
+    def run(self, app, host: str, port: int, debug: bool, **kwargs) -> None:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def make_response(self, data, mimetype=None, content_type=None):
+    def make_response(self, data, mimetype=None, content_type=None) -> Any:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def jsonify(self, obj):
+    def jsonify(self, obj) -> Any:  # pragma: no cover - interface
         pass
 
     @abstractmethod
-    def get_request_adapter(self):
+    def get_request_adapter(self) -> Any:  # pragma: no cover - interface
         pass
