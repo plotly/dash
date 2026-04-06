a7be3fc4f0bdd25c207c519f2e59ceef9f4ab0f1
Adrian Borrmann <hello@adrianborrmann.com>
2026-04-06 13:41:38 -0600

    Make NumberType import backwards-compatible in generated components

--- dash/development/_py_components_generation.py
+++ dash/development/_py_components_generation.py
@@ -24,7 +24,12 @@ import_string = """# AUTO GENERATED FILE - DO NOT EDIT
 import typing  # noqa: F401
 from typing_extensions import TypedDict, NotRequired, Literal # noqa: F401
 from dash.development.base_component import Component, _explicitize_args
-from dash.types import NumberType  # noqa: F401
+try:
+    from dash.types import NumberType  # noqa: F401
+except ImportError:
+    NumberType = typing.Union[  # noqa: F401
+        typing.SupportsFloat, typing.SupportsInt, typing.SupportsComplex
+    ]
 {custom_imports}
 ComponentSingleType = typing.Union[str, int, float, Component, None]
 ComponentType = typing.Union[
