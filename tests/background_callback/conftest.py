f1fe60ebdf1d5b6e9373b9380abc306b0d89fe20
datenzauberai <bierhance@gmx.de>
2026-08-03 17:40:12 +0200

    Do not set redis url on test set-up.

--- tests/background_callback/conftest.py
+++ tests/background_callback/conftest.py
@@ -2,12 +2,10 @@ import os
 
 import pytest
 
-os.environ["REDIS_URL"] = "redis://localhost:6379"
-
 if "REDIS_URL" in os.environ:
     managers = ["celery-filesystem", "celery-redis", "diskcache"]
 else:
-    print("Skipping celery tests because REDIS_URL is not defined")
+    print("Skipping celery tests on Redis because REDIS_URL is not defined")
     managers = ["celery-filesystem", "diskcache"]
 
 
