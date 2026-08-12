2beeaa742f634336f075b210dcf08fce5c23cff2
datenzauberai <bierhance@gmx.de>
2026-08-12 16:46:41 +0200

    Formatting

--- dash/backends/_fastapi.py
+++ dash/backends/_fastapi.py
@@ -753,9 +753,9 @@ class FastAPIDashServer(BaseDashServer[FastAPI]):
             )
             # Track pending callbacks: concurrent.futures.Future (sync/threadpool)
             # or asyncio.Task (async/event-loop).
-            pending_callbacks: Dict[str, concurrent.futures.Future | asyncio.Future] = (
-                {}
-            )
+            pending_callbacks: Dict[
+                str, concurrent.futures.Future | asyncio.Future
+            ] = {}
 
             # Start sender task to drain outbound queue (sends pre-serialized text)
             # pylint: disable=protected-access
